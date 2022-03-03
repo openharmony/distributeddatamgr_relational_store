b523bc6c462e499071c4d8cfa6e36fa98ce3875f
PaDaBoo <xuejianwu@huawei.com>
2022-03-03 20:44:16 +0800

    修改冲突文件
    Signed-off-by: PaDaBoo <xuejianwu@huawei.com>

--- frameworks/innerkitsimpl/native_rdb/src/rdb_store_config.cpp
+++ frameworks/innerkitsimpl/native_rdb/src/rdb_store_config.cpp
@@ -181,7 +181,6 @@ void RdbStoreConfig::ClearEncryptKey()
     encryptKey.clear();
 }
 
-<<<<<<< HEAD
 int RdbStoreConfig::SetDistributedType(DistributedType type)
 {
     if (type < DistributedType::RDB_DEVICE_COLLABORATION || type >= DistributedType::RDB_DISTRIBUTED_TYPE_MAX) {
@@ -192,8 +191,6 @@ int RdbStoreConfig::SetDistributedType(DistributedType type)
     return E_OK;
 }
 
-=======
->>>>>>> 74cf32221d65bf6c5cbdc29a660ede6f89971ce0
 DistributedType RdbStoreConfig::GetDistributedType() const
 {
     return distributedType_;
