cf15f82fe05a58f63d94bb6c2a4cfa4b2004b273
lijuntao <lijuntao9@huawei.com>
2022-03-16 14:33:08 +0800

    增加system.storage接口空实现 Signed-off-by: lijuntao <lijuntao9@huawei.com>

--- frameworks/jskitsimpl/napi_preferences/src/entry_point_system_storage.cpp
+++ frameworks/jskitsimpl/napi_preferences/src/entry_point_system_storage.cpp
@@ -16,32 +16,27 @@
 #include "napi/native_node_api.h"
 #include "napi_system_storage.h"
 
-using namespace OHOS::SystemStorageJsKit;
-
 EXTERN_C_START
 /*
  * function for module exports
  */
 static napi_value Init(napi_env env, napi_value exports)
 {
-OHOS::SystemStorageJsKit::InitSystemStorage(env, exports);
-
-return exports;
+    OHOS::SystemStorageJsKit::InitSystemStorage(env, exports);
+    return exports;
 }
 EXTERN_C_END
 
 /*
  * Module define
  */
-static napi_module _module = {
-    .nm_version = 1,
+static napi_module _module = { .nm_version = 1,
     .nm_flags = 0,
     .nm_filename = nullptr,
     .nm_register_func = Init,
     .nm_modname = "storage",
     .nm_priv = ((void *)0),
-    .reserved = { 0 }
-};
+    .reserved = { 0 } };
 /*
  * Module register function
  */
