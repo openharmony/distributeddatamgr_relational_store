28d9446338dcb226a706c0de978d2997662f6198
PaDoBoo <xuejianwu@huawei.com>
2022-03-18 19:40:16 +0800

    Fix Kora bugs
    Signed-off-by: PaDoBoo <xuejianwu@huawei.com>

--- frameworks/innerkitsimpl/native_rdb/src/values_bucket.cpp
+++ frameworks/innerkitsimpl/native_rdb/src/values_bucket.cpp
@@ -122,11 +122,14 @@ ValuesBucket *ValuesBucket::Unmarshalling(Parcel &parcel)
 {
     int mapSize = parcel.ReadInt32();
     std::map<std::string, ValueObject> valuesMap;
-    for (int i = 0; i < mapSize; i++) {
+    ValueObject *value = nullptr;
+    for (int i = 0; i < mapSize; i++)
+    {
         std::string key = parcel.ReadString();
-        ValueObject *value = parcel.ReadParcelable<ValueObject>();
+        value = parcel.ReadParcelable<ValueObject>();
         valuesMap.insert(std::make_pair(key, *value));
     }
+    value = nullptr;
     return new ValuesBucket(valuesMap);
 }
 } // namespace NativeRdb
