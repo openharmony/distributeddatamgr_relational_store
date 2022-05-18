900a3fcba5041f9ddbb1d7c4be2913f9a48cff3e
duanweiling <duanweiling@huawei.com>
2022-05-18 14:28:58 +0800

    【DataShare】修改update、query参数顺序
    Signed-off-by: duanweiling <duanweiling@huawei.com>

--- frameworks/native/data_share/provider/include/datashare_ext_ability.h
+++ frameworks/native/data_share/provider/include/datashare_ext_ability.h
@@ -115,13 +115,13 @@ public:
      * @brief Updates data records in the database.
      *
      * @param uri Indicates the path of data to update.
-     * @param value Indicates the data to update. This parameter can be null.
      * @param predicates Indicates filter criteria. You should define the processing logic when this parameter is null.
+     * @param value Indicates the data to update. This parameter can be null.
      *
      * @return Returns the number of data records updated.
      */
-    virtual int Update(const Uri &uri, const DataShareValuesBucket &value,
-        const DataSharePredicates &predicates);
+    virtual int Update(const Uri &uri, const DataSharePredicates &predicates,
+        const DataShareValuesBucket &value);
 
     /**
      * @brief Deletes one or more data records from the database.
@@ -137,13 +137,13 @@ public:
      * @brief Deletes one or more data records from the database.
      *
      * @param uri Indicates the path of data to query.
-     * @param columns Indicates the columns to query. If this parameter is null, all columns are queried.
      * @param predicates Indicates filter criteria. You should define the processing logic when this parameter is null.
+     * @param columns Indicates the columns to query. If this parameter is null, all columns are queried.
      *
      * @return Returns the query result.
      */
     virtual std::shared_ptr<DataShareAbstractResultSet> Query(
-        const Uri &uri, std::vector<std::string> &columns, const DataSharePredicates &predicates);
+        const Uri &uri, const DataSharePredicates &predicates, std::vector<std::string> &columns);
 
     /**
      * @brief Obtains the MIME type matching the data specified by the URI of the Data ability. This method should be
