79bf0772618424fb7c09925dc3ef0fec9abd2d61
songruirui <songruirui1@huawei.com>
2022-05-23 15:49:40 +0800

    codex fix
    Signed-off-by: songruirui <songruirui1@huawei.com>

--- interfaces/inner_api/native/data_share/common/include/datashare_predicates.h
+++ interfaces/inner_api/native/data_share/common/include/datashare_predicates.h
@@ -27,7 +27,7 @@ namespace OHOS {
 namespace DataShare {
 class DataSharePredicates : public virtual DataShareAbsPredicates, public virtual OHOS::Parcelable {
 public:
-    DataSharePredicates();
+    DataSharePredicates() = default;
     explicit DataSharePredicates(Predicates &predicates);
     ~DataSharePredicates();
     DataSharePredicates *EqualTo(const std::string &field, const int value)override;
@@ -107,9 +107,8 @@ private:
     std::string whereClause_;
     std::vector<std::string> whereArgs_;
     std::string order_;
-    SettingMode settingMode_;
+    SettingMode settingMode_ = INVALID_MODE;  
 };
 } // namespace DataShare
 } // namespace OHOS
-
 #endif
