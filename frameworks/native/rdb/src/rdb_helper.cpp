0f7b6294f42801bfc62bfab88314be9ddf3050bc
dulei <dulei1@huawei.com>
2022-06-08 13:21:32 +0800

    添加HiTrace追踪
    Signed-off-by: dulei <dulei1@huawei.com>

--- frameworks/native/rdb/src/rdb_helper.cpp
+++ frameworks/native/rdb/src/rdb_helper.cpp
@@ -29,7 +29,7 @@ std::map<std::string, std::shared_ptr<RdbStore>> RdbHelper::storeCache_;
 std::shared_ptr<RdbStore> RdbHelper::GetRdbStore(
     const RdbStoreConfig &config, int version, RdbOpenCallback &openCallback, int &errCode)
 {
-    DistributedKv::DdsTrace trace(std::string(LOG_TAG "::", __FUNCTION__),
+    DistributedKv::DdsTrace trace(std::string(LOG_TAG "::") + std::string(__FUNCTION__),
         DistributedKv::TraceSwitch::BYTRACE_ON | DistributedKv::TraceSwitch::TRACE_CHAIN_ON);
     SqliteGlobalConfig::InitSqliteGlobalConfig();
     std::shared_ptr<RdbStore> rdbStore;
@@ -101,7 +101,7 @@ void RdbHelper::ClearCache()
 
 int RdbHelper::DeleteRdbStore(const std::string &dbFileName)
 {
-    DistributedKv::DdsTrace trace(std::string(LOG_TAG "::", __FUNCTION__));
+    DistributedKv::DdsTrace trace(std::string(LOG_TAG "::") + std::string(__FUNCTION__));
     if (dbFileName.empty()) {
         return E_EMPTY_FILE_NAME;
     }
