337133297bf521126dd113ebe41a367bcfefee25
wanning.fang <songruirui1@huawei.com>
2022-07-07 10:23:00 +0800

    code fix
    Signed-off-by: wanning.fang <songruirui1@huawei.com>

--- data_share/interfaces/inner_api/consumer/include/datashare_helper.h
+++ data_share/interfaces/inner_api/consumer/include/datashare_helper.h
@@ -245,7 +245,6 @@ private:
     Uri uri_ = Uri("");
     sptr<IDataShare> dataShareProxy_ = nullptr;
     static std::mutex oplock_;
-    std::mutex lock_;
     sptr<IRemoteObject::DeathRecipient> callerDeathRecipient_ = nullptr;
     std::map<sptr<AAFwk::IDataAbilityObserver>, sptr<IDataShare>> registerMap_;
     std::map<sptr<AAFwk::IDataAbilityObserver>, std::string> uriMap_;
