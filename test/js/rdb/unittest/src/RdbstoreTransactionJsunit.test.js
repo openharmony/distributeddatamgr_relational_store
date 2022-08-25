ab433c3d30761e4b05bc6512c33ebe046d4aa92d
ltdong <donglongtao@huawei.com>
2022-08-25 15:18:10 +0800

    preferences/rdb adapt for previewer on windows and mac
    Signed-off-by: ltdong <donglongtao@huawei.com>

--- test/js/rdb/unittest/src/RdbstoreTransactionJsunit.test.js
+++ test/js/rdb/unittest/src/RdbstoreTransactionJsunit.test.js
@@ -20,12 +20,12 @@ const TAG = "[RDB_JSKITS_TEST]"
 const CREATE_TABLE_TEST = "CREATE TABLE IF NOT EXISTS test (" + "id INTEGER PRIMARY KEY, " + "name TEXT NOT NULL, " + "age INTEGER, " + "salary REAL, " + "blobType BLOB)";
 
 const STORE_CONFIG = {
-    name: "TransactionTest.db",
+    name: "InsertTest.db",
 }
 
 var rdbStore = undefined;
 
-describe('rdbStoreTransactionTest', function () {
+describe('rdbStoreInsertTest', function () {
     beforeAll(async function () {
         console.info(TAG + 'beforeAll')
         rdbStore = await dataRdb.getRdbStore(STORE_CONFIG, 1);
@@ -45,7 +45,7 @@ describe('rdbStoreTransactionTest', function () {
     afterAll(async function () {
         console.info(TAG + 'afterAll')
         rdbStore = null
-        await dataRdb.deleteRdbStore("TransactionTest.db");
+        await dataRdb.deleteRdbStore("InsertTest.db");
     })
 
     console.log(TAG + "*************Unit Test Begin*************");
