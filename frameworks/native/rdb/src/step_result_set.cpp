60993549c3a9d2a4a16dd6750829cb4a7bff3210
srr101 <songruirui1@huawei.com>
2022-11-03 09:24:14 +0800

    增加finishstep
    Signed-off-by: srr101 <songruirui1@huawei.com>

--- frameworks/native/rdb/src/step_result_set.cpp
+++ frameworks/native/rdb/src/step_result_set.cpp
@@ -48,6 +48,7 @@ int StepResultSet::GetAllColumnNames(std::vector<std::string> &columnNames)
     int columnCount = 0;
     errCode = sqliteStatement->GetColumnCount(columnCount);
     if (errCode) {
+        FinishStep();
         return errCode;
     }
 
@@ -57,11 +58,12 @@ int StepResultSet::GetAllColumnNames(std::vector<std::string> &columnNames)
         errCode = sqliteStatement->GetColumnName(i, columnName);
         if (errCode) {
             columnNames.clear();
+            FinishStep();
             return errCode;
         }
         columnNames.push_back(columnName);
     }
-
+    FinishStep();
     return E_OK;
 }
 
@@ -162,6 +164,7 @@ int StepResultSet::GoToNextRow()
         // The table is locked, retry
         if (retryCount > STEP_QUERY_RETRY_MAX_TIMES) {
             LOG_ERROR("StepResultSet::GoToNextRow retrycount exceeded");
+            FinishStep();
             return E_STEP_RESULT_QUERY_EXCEEDED;
         } else {
             // Sleep to give the thread holding the lock a chance to finish
@@ -173,6 +176,7 @@ int StepResultSet::GoToNextRow()
 
     if (errCode == SQLITE_ROW) {
         rowPos_++;
+        FinishStep();
         return E_OK;
     } else if (errCode == SQLITE_DONE) {
         isAfterLast = true;
