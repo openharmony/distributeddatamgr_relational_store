3bcf5c46f7bc8be9f77fccdae0525b0b2e752e27
lihuihui <lihuihui29@huawei.com>
2023-02-11 09:56:38 +0800

    int 64
    Signed-off-by: lihuihui <lihuihui29@huawei.com>

--- test/js/relationalstore/unittest/src/RdbstoreInsertJsunit.test.js
+++ test/js/relationalstore/unittest/src/RdbstoreInsertJsunit.test.js
@@ -284,6 +284,57 @@ describe('rdbStoreInsertTest', function () {
     })
 
     /**
+     * @tc.name rdb insert test
+     * @tc.number SUB_DDM_AppDataFWK_JSRDB_Insert_0007
+     * @tc.desc rdb insert test
+     */
+    it('testRdbStoreInsert0007', 0, async function (done) {
+        console.log(TAG + "************* testRdbStoreInsert0007 start *************");
+        var u8 = new Uint8Array([1, 2, 3])
+        {
+            const valueBucket = {
+                "name": "zhangsan",
+                "age": 9007199254740991,
+                "salary": 9007199254740990.123,
+                "blobType": u8,
+            }
+            await rdbStore.insert("test", valueBucket)
+        }
+
+        {
+            const valueBucket = {
+                "name": "lisi",
+                "age": -9007199254740991,
+                "salary": -9007199254740990.123,
+                "blobType": u8,
+            }
+            await rdbStore.insert("test", valueBucket)
+        }
+
+        let predicates = new data_relationalStore.RdbPredicates("test");
+        let resultSet = await rdbStore.query(predicates)
+        try {
+            console.log(TAG + "resultSet query done");
+            expect(true).assertEqual(resultSet.goToFirstRow())
+            const age = resultSet.getLong(resultSet.getColumnIndex("age"))
+            const salary = resultSet.getDouble(resultSet.getColumnIndex("salary"))
+            expect(9007199254740991).assertEqual(age)
+            expect(9007199254740990.123).assertEqual(salary)
+
+            expect(true).assertEqual(resultSet.goToNextRow())
+            const age_1 = resultSet.getLong(resultSet.getColumnIndex("age"))
+            const salary_1 = resultSet.getDouble(resultSet.getColumnIndex("salary"))
+            expect(-9007199254740991).assertEqual(age_1)
+            expect(-9007199254740990.123).assertEqual(salary_1)
+            done()
+        } catch (e) {
+            console.log("insert error " + e);
+        }
+        resultSet = null
+        console.log(TAG + "************* testRdbStoreInsert0007 end *************");
+    })
+
+    /**
      * @tc.name rdb inserttWithConflictResolution test
      * @tc.number SUB_DDM_AppDataFWK_JSRDB_InsertWithConflictResolution_0001
      * @tc.desc rdb insertWithConflictResolution test
--- test/native/rdb_data_share_adapter/unittest/rdb_data_share_adapter_test.cpp
+++ test/native/rdb_data_share_adapter/unittest/rdb_data_share_adapter_test.cpp
@@ -368,3 +368,55 @@ HWTEST_F(RdbDataShareAdapterTest, Rdb_DataShare_Adapter_006, TestSize.Level1)
     EXPECT_EQ(blobData2, blobValue[1]);
     EXPECT_EQ(blobData3, blobValue[2]);
 }
+
+/* *
+ * @tc.name: Rdb_DataShare_Adapter_007
+ * @tc.desc: normal testcase of RdbDataShareAdapter
+ * @tc.type: FUNC
+ */
+HWTEST_F(RdbDataShareAdapterTest, Rdb_DataShare_Adapter_007, TestSize.Level1)
+{
+    std::string createTableSql = std::string("CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,") +
+                                 std::string("age INTEGER, salary REAL);");
+    store->ExecuteSql(createTableSql);
+
+    DataShareValuesBucket values;
+    int64_t id;
+    values.Put("name", std::string("zhangsan"));
+    values.Put("age", 9223372036854775807);
+    values.Put("salary", 1.79769e+308);
+    int ret1 = store->Insert(id, "test", RdbUtils::ToValuesBucket(values));
+    EXPECT_EQ(ret1, OHOS::NativeRdb::E_OK);
+    EXPECT_EQ(1, id);
+
+    values.Clear();
+    values.Put("name", std::string("lisi"));
+    values.Put("age", -9223372036854775807i - 1i);
+    values.Put("salary", 2.22507e-308);
+    int ret2 = store->Insert(id, "test", RdbUtils::ToValuesBucket(values));
+    EXPECT_EQ(ret2, OHOS::NativeRdb::E_OK);
+    EXPECT_EQ(2, id);
+
+
+    std::string table = "test";
+    OHOS::DataShare::DataSharePredicates predicates;
+    std::vector<std::string> columns;
+    std::shared_ptr<AbsSharedResultSet> allDataTypes = store->Query(RdbUtils::ToPredicates(predicates, table), columns);
+
+    allDataTypes->GoToFirstRow();
+    int64_t int64Value;
+    allDataTypes->GetLong(2, int64Value);
+    EXPECT_EQ(int64Value, 9223372036854775807);
+
+    double doubleVal;
+    allDataTypes->GetDouble(3, doubleVal);
+    EXPECT_EQ(1.79769e+308, doubleVal);
+
+    allDataTypes->GoToNextRow();
+
+    allDataTypes->GetLong(2, int64Value);
+    EXPECT_EQ(int64Value, -9223372036854775807i - 1i);
+
+    allDataTypes->GetDouble(3, doubleVal);
+    EXPECT_EQ(2.22507e-308, doubleVal);
+}
