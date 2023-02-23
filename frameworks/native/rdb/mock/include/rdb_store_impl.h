01199126146b8b11450b86fd98b6cc7df74b95f6
lihuihui <lihuihui29@huawei.com>
2023-02-23 10:45:52 +0800

    innerapi3
    Signed-off-by: lihuihui <lihuihui29@huawei.com>

--- frameworks/native/rdb/mock/include/rdb_store_impl.h
+++ frameworks/native/rdb/mock/include/rdb_store_impl.h
@@ -30,67 +30,66 @@
 #include "transaction_observer.h"
 
 namespace OHOS::NativeRdb {
-class API_EXPORT RdbStoreImpl : public RdbStore, public std::enable_shared_from_this<RdbStoreImpl> {
+class RdbStoreImpl : public RdbStore, public std::enable_shared_from_this<RdbStoreImpl> {
 public:
-    API_EXPORT static std::shared_ptr<RdbStore> Open(const RdbStoreConfig &config, int &errCode);
-    API_EXPORT RdbStoreImpl();
-    API_EXPORT ~RdbStoreImpl() override;
+    static std::shared_ptr<RdbStore> Open(const RdbStoreConfig &config, int &errCode);
+    RdbStoreImpl();
+    ~RdbStoreImpl() override;
 #ifdef WINDOWS_PLATFORM
-    API_EXPORT void Clear() override;
+    void Clear() override;
 #endif
-    API_EXPORT int Insert(int64_t &outRowId, const std::string &table, const ValuesBucket &initialValues) override;
-    API_EXPORT int BatchInsert(int64_t &outInsertNum, const std::string &table,
+    int Insert(int64_t &outRowId, const std::string &table, const ValuesBucket &initialValues) override;
+    int BatchInsert(int64_t &outInsertNum, const std::string &table,
         const std::vector<ValuesBucket> &initialBatchValues) override;
-    API_EXPORT int Replace(int64_t &outRowId, const std::string &table, const ValuesBucket &initialValues) override;
-    API_EXPORT int InsertWithConflictResolution(int64_t &outRowId, const std::string &table,
-        const ValuesBucket &initialValues, ConflictResolution conflictResolution) override;
-    API_EXPORT int Update(int &changedRows, const std::string &table, const ValuesBucket &values,
-        const std::string &whereClause, const std::vector<std::string> &whereArgs) override;
-    API_EXPORT int UpdateWithConflictResolution(int &changedRows, const std::string &table, const ValuesBucket &values,
+    int Replace(int64_t &outRowId, const std::string &table, const ValuesBucket &initialValues) override;
+    int InsertWithConflictResolution(int64_t &outRowId, const std::string &table, const ValuesBucket &initialValues,
+        ConflictResolution conflictResolution) override;
+    int Update(int &changedRows, const std::string &table, const ValuesBucket &values, const std::string &whereClause,
+        const std::vector<std::string> &whereArgs) override;
+    int UpdateWithConflictResolution(int &changedRows, const std::string &table, const ValuesBucket &values,
         const std::string &whereClause, const std::vector<std::string> &whereArgs,
         ConflictResolution conflictResolution) override;
-    API_EXPORT int Delete(int &deletedRows, const std::string &table, const std::string &whereClause,
+    int Delete(int &deletedRows, const std::string &table, const std::string &whereClause,
         const std::vector<std::string> &whereArgs) override;
-    API_EXPORT int ExecuteSql(const std::string &sql, const std::vector<ValueObject> &bindArgs) override;
-    API_EXPORT int ExecuteAndGetLong(
-        int64_t &outValue, const std::string &sql, const std::vector<ValueObject> &bindArgs) override;
-    API_EXPORT int ExecuteAndGetString(std::string &outValue, const std::string &sql,
+    int ExecuteSql(const std::string &sql, const std::vector<ValueObject> &bindArgs) override;
+    int ExecuteAndGetLong(int64_t &outValue, const std::string &sql, const std::vector<ValueObject> &bindArgs) override;
+    int ExecuteAndGetString(std::string &outValue, const std::string &sql,
         const std::vector<ValueObject> &bindArgs) override;
-    API_EXPORT int ExecuteForLastInsertedRowId(int64_t &outValue, const std::string &sql,
+    int ExecuteForLastInsertedRowId(int64_t &outValue, const std::string &sql,
         const std::vector<ValueObject> &bindArgs) override;
-    API_EXPORT int ExecuteForChangedRowCount(int64_t &outValue, const std::string &sql,
+    int ExecuteForChangedRowCount(int64_t &outValue, const std::string &sql,
         const std::vector<ValueObject> &bindArgs) override;
-    API_EXPORT int Backup(const std::string databasePath, const std::vector<uint8_t> destEncryptKey) override;
-    API_EXPORT int Attach(const std::string &alias, const std::string &pathName,
+    int Backup(const std::string databasePath, const std::vector<uint8_t> destEncryptKey) override;
+    int Attach(const std::string &alias, const std::string &pathName,
         const std::vector<uint8_t> destEncryptKey) override;
-    API_EXPORT int GetVersion(int &version) override;
-    API_EXPORT int SetVersion(int version) override;
-    API_EXPORT int BeginTransaction() override;
-    API_EXPORT int RollBack() override;
-    API_EXPORT int Commit() override;
-    API_EXPORT bool IsInTransaction() override;
-    API_EXPORT bool IsOpen() const override;
-    API_EXPORT std::string GetPath() override;
-    API_EXPORT bool IsReadOnly() const override;
-    API_EXPORT bool IsMemoryRdb() const override;
-    API_EXPORT bool IsHoldingConnection() override;
-    API_EXPORT int GiveConnectionTemporarily(int64_t milliseconds);
+    int GetVersion(int &version) override;
+    int SetVersion(int version) override;
+    int BeginTransaction() override;
+    int RollBack() override;
+    int Commit() override;
+    bool IsInTransaction() override;
+    bool IsOpen() const override;
+    std::string GetPath() override;
+    bool IsReadOnly() const override;
+    bool IsMemoryRdb() const override;
+    bool IsHoldingConnection() override;
+    int GiveConnectionTemporarily(int64_t milliseconds);
 #ifdef RDB_SUPPORT_ICU
-    API_EXPORT int ConfigLocale(const std::string localeStr);
+    int ConfigLocale(const std::string localeStr);
 #endif
-    API_EXPORT int Restore(const std::string backupPath, const std::vector<uint8_t> &newKey) override;
-    API_EXPORT int ChangeDbFileForRestore(const std::string newPath, const std::string backupPath,
+    int Restore(const std::string backupPath, const std::vector<uint8_t> &newKey) override;
+    int ChangeDbFileForRestore(const std::string newPath, const std::string backupPath,
         const std::vector<uint8_t> &newKey) override;
-    API_EXPORT std::string GetName();
-    API_EXPORT std::string GetOrgPath();
-    API_EXPORT std::string GetFileType();
-    API_EXPORT std::unique_ptr<ResultSet> QueryByStep(const std::string &sql,
+    std::string GetName();
+    std::string GetOrgPath();
+    std::string GetFileType();
+    std::unique_ptr<ResultSet> QueryByStep(const std::string &sql,
         const std::vector<std::string> &selectionArgs) override;
-    API_EXPORT std::unique_ptr<ResultSet> Query(
+    std::unique_ptr<ResultSet> Query(
         const AbsRdbPredicates &predicates, const std::vector<std::string> columns) override;
-    API_EXPORT int Count(int64_t &outValue, const AbsRdbPredicates &predicates) override;
-    API_EXPORT int Update(int &changedRows, const ValuesBucket &values, const AbsRdbPredicates &predicates) override;
-    API_EXPORT int Delete(int &deletedRows, const AbsRdbPredicates &predicates) override;
+    int Count(int64_t &outValue, const AbsRdbPredicates &predicates) override;
+    int Update(int &changedRows, const ValuesBucket &values, const AbsRdbPredicates &predicates) override;
+    int Delete(int &deletedRows, const AbsRdbPredicates &predicates) override;
 
 private:
     int InnerOpen(const RdbStoreConfig &config);
