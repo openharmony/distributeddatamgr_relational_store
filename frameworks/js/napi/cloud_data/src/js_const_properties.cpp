14613a9fb9be0513fffd4025ad43d3edb5fae5d4
ylq121 <yangqing89@huawei.com>
2023-05-13 11:47:49 +0800

    feat:cloudSync napi
    Signed-off-by: ylq121 <yangqing89@huawei.com>

---
+++ frameworks/js/napi/cloud_data/src/js_const_properties.cpp
@@ -0,0 +1,53 @@
+/*
+* Copyright (c) 2023 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#define LOG_TAG "Const_Properties"
+#include "js_const_properties.h"
+
+#include "cloud_service.h"
+#include "log_print.h"
+#include "napi_queue.h"
+
+using Action = OHOS::CloudData::CloudService::Action;
+namespace OHOS::CloudData {
+static napi_status SetNamedProperty(napi_env env, napi_value &obj, const std::string &name, int32_t value)
+{
+    napi_value property = nullptr;
+    napi_status status = napi_create_int32(env, value, &property);
+    ASSERT(status == napi_ok, "int32_t to napi_value failed!", status);
+    status = napi_set_named_property(env, obj, name.c_str(), property);
+    ASSERT(status == napi_ok, "napi_set_named_property failed!", status);
+    return status;
+}
+
+static napi_value ExportAction(napi_env env)
+{
+    napi_value action = nullptr;
+    napi_create_object(env, &action);
+    SetNamedProperty(env, action, "CLEAR_CLOUD_INFO", (int32_t)Action::CLEAR_CLOUD_INFO);
+    SetNamedProperty(env, action, "CLEAR_CLOUD_DATA_AND_INFO", (int32_t)Action::CLEAR_CLOUD_DATA_AND_INFO);
+    napi_object_freeze(env, action);
+    return action;
+}
+
+napi_status InitConstProperties(napi_env env, napi_value exports)
+{
+    const napi_property_descriptor properties[] = {
+        DECLARE_NAPI_PROPERTY("Action", ExportAction(env)),
+    };
+    size_t count = sizeof(properties) / sizeof(properties[0]);
+
+    return napi_define_properties(env, exports, count, properties);
+}
+} // namespace OHOS::CloudData
---
+++ frameworks/js/napi/cloud_data/src/napi_queue.cpp
@@ -0,0 +1,163 @@
+/*
+* Copyright (c) 2023 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#define LOG_TAG "NapiQueue"
+#include "napi_queue.h"
+
+namespace OHOS::CloudData {
+ContextBase::~ContextBase()
+{
+    ZLOGD("no memory leak after callback or promise[resolved/rejected]");
+    if (env != nullptr) {
+        if (callbackRef != nullptr) {
+            auto status = napi_delete_reference(env, callbackRef);
+            ZLOGD("status:%{public}d", status);
+        }
+        if (selfRef != nullptr) {
+            auto status = napi_delete_reference(env, selfRef);
+            ZLOGD("status:%{public}d", status);
+        }
+        env = nullptr;
+    }
+}
+
+void ContextBase::GetCbInfo(napi_env envi, napi_callback_info info, NapiCbInfoParser parse, bool sync)
+{
+    env = envi;
+    size_t argc = ARGC_MAX;
+    napi_value argv[ARGC_MAX] = { nullptr };
+    status = napi_get_cb_info(env, info, &argc, argv, &self, nullptr);
+    ASSERT_STATUS(this, "napi_get_cb_info failed!");
+    ASSERT_ARGS(this, argc <= ARGC_MAX, "too many arguments!");
+    ASSERT_ARGS(this, self != nullptr, "no JavaScript this argument!");
+    if (!sync) {
+        napi_create_reference(env, self, 1, &selfRef);
+    }
+    status = napi_unwrap(env, self, &native);
+    ASSERT_STATUS(this, "self unwrap failed!");
+
+    if (!sync && (argc > 0)) {
+        // get the last arguments :: <callback>
+        size_t index = argc - 1;
+        napi_valuetype type = napi_undefined;
+        napi_status tyst = napi_typeof(env, argv[index], &type);
+        if ((tyst == napi_ok) && (type == napi_function)) {
+            status = napi_create_reference(env, argv[index], 1, &callbackRef);
+            ASSERT_STATUS(this, "ref callback failed!");
+            argc = index;
+            ZLOGD("async callback, no promise");
+        } else {
+            ZLOGD("no callback, async pormose");
+        }
+    }
+
+    if (parse) {
+        parse(argc, argv);
+    } else {
+        ASSERT_ARGS(this, argc == 0, "required no arguments!");
+    }
+}
+
+napi_value NapiQueue::AsyncWork(napi_env env, std::shared_ptr<ContextBase> ctxt, const std::string &name,
+    NapiAsyncExecute execute, NapiAsyncComplete complete)
+{
+    ZLOGD("name=%{public}s", name.c_str());
+    AsyncContext *aCtx = new AsyncContext;
+    aCtx->env = env;
+    aCtx->ctx = std::move(ctxt);
+    aCtx->execute = std::move(execute);
+    aCtx->complete = std::move(complete);
+    napi_value promise = nullptr;
+    if (aCtx->ctx->callbackRef == nullptr) {
+        napi_create_promise(env, &aCtx->deferred, &promise);
+        ZLOGD("create deferred promise");
+    } else {
+        napi_get_undefined(env, &promise);
+    }
+
+    napi_value resource = nullptr;
+    napi_create_string_utf8(env, name.c_str(), NAPI_AUTO_LENGTH, &resource);
+    napi_create_async_work(
+        env, nullptr, resource,
+        [](napi_env env, void *data) {
+            ASSERT_VOID(data != nullptr, "napi_async_execute_callback nullptr");
+            auto actx = reinterpret_cast<AsyncContext *>(data);
+            ZLOGD("napi_async_execute_callback ctxt->status=%{public}d", actx->ctx->status);
+            if (actx->execute && actx->ctx->status == napi_ok) {
+                actx->execute();
+            }
+        },
+        [](napi_env env, napi_status status, void *data) {
+            ASSERT_VOID(data != nullptr, "napi_async_complete_callback nullptr");
+            auto actx = reinterpret_cast<AsyncContext *>(data);
+            ZLOGD("napi_async_complete_callback status=%{public}d, ctxt->status=%{public}d", status, actx->ctx->status);
+            if ((status != napi_ok) && (actx->ctx->status == napi_ok)) {
+                actx->ctx->status = status;
+            }
+            napi_value output = nullptr;
+            if ((actx->complete) && (status == napi_ok) && (actx->ctx->status == napi_ok)) {
+                actx->complete(output);
+            }
+            GenerateOutput(*actx, output);
+            delete actx;
+        },
+        reinterpret_cast<void *>(aCtx), &aCtx->work);
+    auto status = napi_queue_async_work(env, aCtx->work);
+    if (status != napi_ok) {
+        napi_get_undefined(env, &promise);
+        delete aCtx;
+    }
+    return promise;
+}
+
+void NapiQueue::GenerateOutput(AsyncContext &ctx, napi_value output)
+{
+    napi_value result[RESULT_ALL] = { nullptr };
+    if (ctx.ctx->status == napi_ok) {
+        napi_get_undefined(ctx.env, &result[RESULT_ERROR]);
+        if (output == nullptr) {
+            napi_get_undefined(ctx.env, &output);
+        }
+        result[RESULT_DATA] = output;
+    } else {
+        napi_value message = nullptr;
+        napi_value errorCode = nullptr;
+        if (ctx.ctx->jsCode != 0 && ctx.ctx->jsCode != -1) {
+            napi_create_string_utf8(ctx.env, std::to_string(ctx.ctx->jsCode).c_str(), NAPI_AUTO_LENGTH, &errorCode);
+        }
+        if (ctx.ctx->jsCode == -1) {
+            std::string jscode = "";
+            napi_create_string_utf8(ctx.env, jscode.c_str(), NAPI_AUTO_LENGTH, &errorCode);
+        }
+        napi_create_string_utf8(ctx.env, ctx.ctx->error.c_str(), NAPI_AUTO_LENGTH, &message);
+        napi_create_error(ctx.env, errorCode, message, &result[RESULT_ERROR]);
+        napi_get_undefined(ctx.env, &result[RESULT_DATA]);
+    }
+    if (ctx.deferred != nullptr) {
+        if (ctx.ctx->status == napi_ok) {
+            ZLOGD("deferred promise resolved");
+            napi_resolve_deferred(ctx.env, ctx.deferred, result[RESULT_DATA]);
+        } else {
+            ZLOGD("deferred promise rejected");
+            napi_reject_deferred(ctx.env, ctx.deferred, result[RESULT_ERROR]);
+        }
+    } else {
+        napi_value callback = nullptr;
+        napi_get_reference_value(ctx.env, ctx.ctx->callbackRef, &callback);
+        napi_value callbackResult = nullptr;
+        ZLOGD("call callback function");
+        napi_call_function(ctx.env, nullptr, callback, RESULT_ALL, result, &callbackResult);
+    }
+}
+} // namespace OHOS::CloudData
