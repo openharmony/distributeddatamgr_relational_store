64c1192f2071606f6e27756d48f440f2b6256f3b
zuojiangjiang <zuojiangjiang@huawei.com>
2023-06-20 22:09:17 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/js/napi/common/src/js_uv_queue.cpp
+++ frameworks/js/napi/common/src/js_uv_queue.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "uv_queue.h"
+#include "js_uv_queue.h"
 #include "logger.h"
 
 namespace OHOS::AppDataMgrJsKit {
