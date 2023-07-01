6b1a85b2ac12a7c0ead45241c7d9183949acf2ca
zuojiangjiang <zuojiangjiang@huawei.com>
2023-07-01 20:21:07 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/native/rdb/src/rdb_store_impl.cpp
+++ frameworks/native/rdb/src/rdb_store_impl.cpp
@@ -117,8 +117,7 @@ std::map<RdbStore::PRIKey, RdbStore::Date> RdbStoreImpl::GetModifyTime(
         LOG_ERROR("invalid para.");
         return {};
     }
-    if (PKey.begin()->GetType() != ValueObject::TypeId::TYPE_INT &&
-        PKey.begin()->GetType() != ValueObject::TypeId::TYPE_DOUBLE &&
+    if (PKey.begin()->GetType() != ValueObject::TypeId::TYPE_DOUBLE &&
         PKey.begin()->GetType() != ValueObject::TypeId::TYPE_STRING) {
         LOG_ERROR("invalid PRIKey type.");
         return {};
