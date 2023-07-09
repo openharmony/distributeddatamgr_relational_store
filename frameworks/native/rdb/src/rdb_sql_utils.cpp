dd097de4da1df44cbe97f708c97347da3b1ef047
MangTsang <mang.tsang@huawei.com>
2023-07-09 16:01:34 +0800

    remove path check
    Signed-off-by: MangTsang <mang.tsang@huawei.com>

--- frameworks/native/rdb/src/rdb_sql_utils.cpp
+++ frameworks/native/rdb/src/rdb_sql_utils.cpp
@@ -14,7 +14,6 @@
  */
 
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include <algorithm>
@@ -22,58 +21,32 @@
 #ifdef WINDOWS_PLATFORM
 #include <dir.h>
 #endif
-#include <climits>
 
-#include "logger.h"
 #include "rdb_errno.h"
 #include "rdb_sql_utils.h"
 #include "sqlite_sql_builder.h"
 
 #ifdef WINDOWS_PLATFORM
-#define REALPATH(relPath, absPath, ...) (_fullpath(absPath, relPath, ##__VA_ARGS__))
 #define MKDIR(filePath) (mkdir(filePath))
 #else
-#define REALPATH(absPath, relPath, ...) (realpath(absPath, relPath))
 #define MKDIR(filePath) (mkdir(filePath, 0771))
 #endif
 
 namespace OHOS {
 namespace NativeRdb {
-using namespace OHOS::Rdb;
 
 /**
  * Get and Check default path.
  */
 std::string RdbSqlUtils::GetDefaultDatabasePath(const std::string &baseDir, const std::string &name, int &errorCode)
 {
-    if (access(baseDir.c_str(), F_OK) != 0) {
-        if (MKDIR(baseDir.c_str())) {
-            errorCode = E_CREATE_FOLDER_FAIL;
-        }
-    }
-#if defined(WINDOWS_PLATFORM)
-    std::string databasePath = baseDir + "\\rdb";
-#else
     std::string databasePath = baseDir + "/rdb";
-#endif
     if (access(databasePath.c_str(), F_OK) != 0) {
         if (MKDIR(databasePath.c_str())) {
             errorCode = E_CREATE_FOLDER_FAIL;
         }
     }
-    char canonicalPath[PATH_MAX + 1] = { 0 };
-    if (REALPATH(databasePath.c_str(), canonicalPath, PATH_MAX) == nullptr) {
-        LOG_ERROR("Failed to obtain real path, errno:%{public}d", errno);
-        errorCode = E_INVALID_FILE_PATH;
-        return "";
-    }
-    std::string realFilePath(canonicalPath);
-#if defined(WINDOWS_PLATFORM)
-    realFilePath = realFilePath.append("\\").append(name);
-#else
-    realFilePath = realFilePath.append("/").append(name);
-#endif
-    return realFilePath;
+    return databasePath.append("/").append(name);
 }
 
 std::string RdbSqlUtils::BuildQueryString(const AbsRdbPredicates &predicates, const std::vector<std::string> &columns)
