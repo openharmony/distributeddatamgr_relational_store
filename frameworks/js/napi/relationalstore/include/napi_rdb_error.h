1b4689bcb862c331a101981fafada32a5fcf86d4
ylq121 <yangqing89@huawei.com>
2023-07-26 20:22:26 +0800

    xiugai
    Signed-off-by: ylq121 <yangqing89@huawei.com>

--- frameworks/js/napi/relationalstore/include/napi_rdb_error.h
+++ frameworks/js/napi/relationalstore/include/napi_rdb_error.h
@@ -44,6 +44,7 @@ const static std::map<int, std::string> ERROR_MAPS = {
     { E_NOT_STAGE_MODE, "Only supported in stage mode." },
     { E_DATA_GROUP_ID_INVALID, "The data group id is invalid." },
     { NativeRdb::E_GET_DATAOBSMGRCLIENT_FAIL, "Failed to get DataObsMgrClient." },
+    { NativeRdb::E_TYPE_MISMATCH, "The type of the distributed table does not match"},
 };
 
 #define RDB_REVT_NOTHING
