c35db68c73d6e83c774f8c16c0e0a43e66d2778f
marui <marui83@huawei.com>
2023-08-03 08:59:54 +0800

    test GetModifyTimeByRowId
    Signed-off-by: marui <marui83@huawei.com>

--- frameworks/js/napi/rdb/mock/include/napi_rdb_store.h
+++ frameworks/js/napi/rdb/mock/include/napi_rdb_store.h
@@ -18,6 +18,7 @@
 
 #include <list>
 #include <mutex>
+#include <gmock/gmock.h>
 
 #include "napi/native_api.h"
 #include "napi/native_common.h"
@@ -66,6 +67,10 @@ private:
     static napi_value SetVersion(napi_env env, napi_callback_info info);
     std::mutex mutex_;
     std::shared_ptr<OHOS::NativeRdb::RdbStore> rdbStore_;
+};
+
+class TestRdbStoreProxy : public RdbStoreProxy {
+     MOCK_CONST_METHOD2(QueryByStep, bool(const std::string&, const std::string&));
 };
 } // namespace RdbJsKit
 } // namespace OHOS
