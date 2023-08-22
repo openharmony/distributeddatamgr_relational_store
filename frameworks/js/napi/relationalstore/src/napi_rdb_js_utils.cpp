ca91f158f1db05fee9c6686e66714e3d09d1aaeb
lihuihui <lihuihui29@huawei.com>
2023-08-22 11:09:29 +0800

    rdb
    Signed-off-by: lihuihui <lihuihui29@huawei.com>

--- frameworks/js/napi/relationalstore/src/napi_rdb_js_utils.cpp
+++ frameworks/js/napi/relationalstore/src/napi_rdb_js_utils.cpp
@@ -233,7 +233,7 @@ napi_value Convert2JSValue(napi_env env, const Date &date)
 }
 
 template<>
-std::string ToString(const PRIKey &key)  //// 字符串类型转换
+std::string ToString(const PRIKey &key)
 {
     auto strVal = std::get_if<std::string>(&key);
     if (strVal != nullptr) {
