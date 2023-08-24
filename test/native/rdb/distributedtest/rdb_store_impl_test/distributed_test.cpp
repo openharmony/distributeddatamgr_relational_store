0a0a45a3681b7fa733aa36a15cbcf99d6ce886d8
marui <marui83@huawei.com>
2023-08-24 14:40:42 +0800

    add tdd 6
    Signed-off-by: marui <marui83@huawei.com>

--- test/native/rdb/distributedtest/rdb_store_impl_test/distributed_test.cpp
+++ test/native/rdb/distributedtest/rdb_store_impl_test/distributed_test.cpp
@@ -26,6 +26,7 @@
 #include "rdb_open_callback.h"
 #include "rdb_store_impl.h"
 #include "rdb_types.h"
+#include "result_set_proxy.h"
 
 #include <regex>
 
@@ -161,6 +162,49 @@ HWTEST_F(DistributedTest, RemoteQuery001, TestSize.Level1)
 
     EXPECT_TRUE(ret > 0);
     EXPECT_EQ(returvalue, "zhangsan");
+}
+
+
+/**
+ * @tc.name: ResultSetProxy001
+ * @tc.desc: Abnormal testcase of distributed ResultSetProxy, if resultSet is Empty
+ * @tc.type: FUNC
+ */
+HWTEST_F(DistributedTest, ResultSetProxy001, TestSize.Level1)
+{
+    int errCode = 0;
+    std::shared_ptr<OHOS::NativeRdb::ResultSetProxy> resultSet;
+    ColumnType columnType;
+    errCode = resultSet->GetColumnType(1, columnType);
+    EXPECT_NE(E_OK, errCode);
+
+    std::string columnName;
+    errCode = resultSet->GetColumnName(1, columnName);
+    EXPECT_NE(E_OK, errCode);
+
+    std::vector<uint8_t> blob;
+    errCode = resultSet->GetBlob(1, blob);
+    EXPECT_NE(E_OK, errCode);
+
+    std::string GetStringvalue;
+    errCode = resultSet->GetString(1, GetStringvalue);
+    EXPECT_NE(E_OK, errCode);
+
+    int GetIntvalue;
+    errCode = resultSet->GetInt(1, GetIntvalue);
+    EXPECT_NE(E_OK, errCode);
+
+    int64_t GetLongvalue;
+    errCode = resultSet->GetLong(1, GetLongvalue);
+    EXPECT_NE(E_OK, errCode);
+
+    double GetDoublevalue;
+    errCode = resultSet->GetDouble(1, GetDoublevalue);
+    EXPECT_NE(E_OK, errCode);
+
+    bool isNull;
+    errCode = resultSet->IsColumnNull(1, isNull);
+    EXPECT_NE(E_OK, errCode);
 }
 }
 
