9c48eb76c0930cf94763743d47a2a4a395de357a
leiiyb <leiyanbo@huawei.com>
2023-09-14 10:29:38 +0800

    solving xts cases for asset fail at cross platform
    Signed-off-by: leiiyb <leiyanbo@huawei.com>

--- test/js/relationalstore/unittest/src/RdbstoreCallBackJsunit.test.js
+++ test/js/relationalstore/unittest/src/RdbstoreCallBackJsunit.test.js
@@ -18,8 +18,8 @@ import data_relationalStore from '@ohos.data.relationalStore';
 import ability_featureAbility from '@ohos.ability.featureAbility';
 
 const TAG = "[RELATIONAL_STORE_CALLBACK_TEST]"
-const CREATE_TABLE_TEST = "CREATE TABLE IF NOT EXISTS test ("
-                          "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+const CREATE_TABLE_TEST = "CREATE TABLE IF NOT EXISTS test (" +
+                          "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                           "name TEXT NOT NULL, age INTEGER, salary REAL, blobType BLOB)";
 
 const STORE_CONFIG = {
