1056fb4d0f33523459aba58e658e362ebb5166fc
zuojiangjiang <zuojiangjiang@huawei.com>
2023-10-25 11:56:41 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/native/rdb/src/rdb_store_impl.cpp
+++ frameworks/native/rdb/src/rdb_store_impl.cpp
@@ -241,7 +241,7 @@ int RdbStoreImpl::Clean(const std::string &table, int64_t cursor)
         sql.append(" AND cursor <= ").append(std::to_string(cursor));
     }
     sql.append(")");
-    return ExecuteSql(table);
+    return ExecuteSql(sql);
 }
 
 #endif
