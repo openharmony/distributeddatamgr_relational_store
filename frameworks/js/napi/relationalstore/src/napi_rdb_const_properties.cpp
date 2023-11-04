b517f94776435db0b96f3ea0924c880a446aebdd
zuojiangjiang <zuojiangjiang@huawei.com>
2023-11-04 17:31:57 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/js/napi/relationalstore/src/napi_rdb_const_properties.cpp
+++ frameworks/js/napi/relationalstore/src/napi_rdb_const_properties.cpp
@@ -41,11 +41,11 @@ static napi_value ExportSyncMode(napi_env env)
     napi_value syncMode = nullptr;
     napi_create_object(env, &syncMode);
 
-    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_PUSH", (int32_t)SyncMode::PUSH);
-    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_PULL", (int32_t)SyncMode::PULL);
-    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_TIME_FIRST", (int32_t)SyncMode::TIME_FIRST);
-    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_NATIVE_FIRST", (int32_t)SyncMode::NATIVE_FIRST);
-    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_CLOUD_FIRST", (int32_t)SyncMode::CLOUD_FIRST);
+    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_PUSH", int32_t(SyncMode::PUSH));
+    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_PULL", int32_t(SyncMode::PULL));
+    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_TIME_FIRST", int32_t(SyncMode::TIME_FIRST));
+    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_NATIVE_FIRST", int32_t(SyncMode::NATIVE_FIRST));
+    SET_NAPI_PROPERTY(syncMode, "SYNC_MODE_CLOUD_FIRST", int32_t(SyncMode::CLOUD_FIRST));
     napi_object_freeze(env, syncMode);
     return syncMode;
 }
@@ -55,9 +55,9 @@ static napi_value ExportSubscribeType(napi_env env)
     napi_value subscribeType = nullptr;
     napi_create_object(env, &subscribeType);
 
-    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_REMOTE", (int32_t)SubscribeMode::REMOTE);
-    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_CLOUD", (int32_t)SubscribeMode::CLOUD);
-    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_CLOUD_DETAILS", (int32_t)SubscribeMode::CLOUD_DETAIL);
+    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_REMOTE", int32_t(SubscribeMode::REMOTE));
+    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_CLOUD", int32_t(SubscribeMode::CLOUD));
+    SET_NAPI_PROPERTY(subscribeType, "SUBSCRIBE_TYPE_CLOUD_DETAILS", int32_t(SubscribeMode::CLOUD_DETAIL));
     napi_object_freeze(env, subscribeType);
     return subscribeType;
 }
@@ -67,10 +67,10 @@ static napi_value ExportSecurityLevel(napi_env env)
     napi_value securityLevel = nullptr;
     napi_create_object(env, &securityLevel);
 
-    SET_NAPI_PROPERTY(securityLevel, "S1", (int32_t)SecurityLevel::S1);
-    SET_NAPI_PROPERTY(securityLevel, "S2", (int32_t)SecurityLevel::S2);
-    SET_NAPI_PROPERTY(securityLevel, "S3", (int32_t)SecurityLevel::S3);
-    SET_NAPI_PROPERTY(securityLevel, "S4", (int32_t)SecurityLevel::S4);
+    SET_NAPI_PROPERTY(securityLevel, "S1", int32_t(SecurityLevel::S1));
+    SET_NAPI_PROPERTY(securityLevel, "S2", int32_t(SecurityLevel::S2));
+    SET_NAPI_PROPERTY(securityLevel, "S3", int32_t(SecurityLevel::S3));
+    SET_NAPI_PROPERTY(securityLevel, "S4", int32_t(SecurityLevel::S4));
     napi_object_freeze(env, securityLevel);
     return securityLevel;
 }
@@ -81,9 +81,9 @@ static napi_value ExportProgress(napi_env env)
     napi_value progress = nullptr;
     napi_create_object(env, &progress);
 
-    SET_NAPI_PROPERTY(progress, "SYNC_BEGIN", (int32_t)DistributedRdb::Progress::SYNC_BEGIN);
-    SET_NAPI_PROPERTY(progress, "SYNC_IN_PROGRESS", (int32_t)DistributedRdb::Progress::SYNC_IN_PROGRESS);
-    SET_NAPI_PROPERTY(progress, "SYNC_FINISH", (int32_t)DistributedRdb::Progress::SYNC_FINISH);
+    SET_NAPI_PROPERTY(progress, "SYNC_BEGIN", int32_t(DistributedRdb::Progress::SYNC_BEGIN));
+    SET_NAPI_PROPERTY(progress, "SYNC_IN_PROGRESS", int32_t(DistributedRdb::Progress::SYNC_IN_PROGRESS));
+    SET_NAPI_PROPERTY(progress, "SYNC_FINISH", int32_t(DistributedRdb::Progress::SYNC_FINISH));
     napi_object_freeze(env, progress);
     return progress;
 }
@@ -93,13 +93,13 @@ static napi_value ExportProgressCode(napi_env env)
     napi_value progressCode = nullptr;
     napi_create_object(env, &progressCode);
 
-    SET_NAPI_PROPERTY(progressCode, "SUCCESS", (int32_t)ProgressCode::SUCCESS);
-    SET_NAPI_PROPERTY(progressCode, "UNKNOWN_ERROR", (int32_t)ProgressCode::UNKNOWN_ERROR);
-    SET_NAPI_PROPERTY(progressCode, "NETWORK_ERROR", (int32_t)ProgressCode::NETWORK_ERROR);
-    SET_NAPI_PROPERTY(progressCode, "CLOUD_DISABLED", (int32_t)ProgressCode::CLOUD_DISABLED);
-    SET_NAPI_PROPERTY(progressCode, "LOCKED_BY_OTHERS", (int32_t)ProgressCode::LOCKED_BY_OTHERS);
-    SET_NAPI_PROPERTY(progressCode, "RECORD_LIMIT_EXCEEDED", (int32_t)ProgressCode::RECORD_LIMIT_EXCEEDED);
-    SET_NAPI_PROPERTY(progressCode, "NO_SPACE_FOR_ASSET", (int32_t)ProgressCode::NO_SPACE_FOR_ASSET);
+    SET_NAPI_PROPERTY(progressCode, "SUCCESS", int32_t(ProgressCode::SUCCESS));
+    SET_NAPI_PROPERTY(progressCode, "UNKNOWN_ERROR", int32_t(ProgressCode::UNKNOWN_ERROR));
+    SET_NAPI_PROPERTY(progressCode, "NETWORK_ERROR", int32_t(ProgressCode::NETWORK_ERROR));
+    SET_NAPI_PROPERTY(progressCode, "CLOUD_DISABLED", int32_t(ProgressCode::CLOUD_DISABLED));
+    SET_NAPI_PROPERTY(progressCode, "LOCKED_BY_OTHERS", int32_t(ProgressCode::LOCKED_BY_OTHERS));
+    SET_NAPI_PROPERTY(progressCode, "RECORD_LIMIT_EXCEEDED", int32_t(ProgressCode::RECORD_LIMIT_EXCEEDED));
+    SET_NAPI_PROPERTY(progressCode, "NO_SPACE_FOR_ASSET", int32_t(ProgressCode::NO_SPACE_FOR_ASSET));
     napi_object_freeze(env, progressCode);
     return progressCode;
 }
@@ -109,9 +109,9 @@ static napi_value ExportOrigin(napi_env env)
     napi_value origin = nullptr;
     napi_create_object(env, &origin);
 
-    SET_NAPI_PROPERTY(origin, "LOCAL", (int32_t)NativeRdb::AbsPredicates::Origin::LOCAL);
-    SET_NAPI_PROPERTY(origin, "CLOUD", (int32_t)NativeRdb::AbsPredicates::Origin::CLOUD);
-    SET_NAPI_PROPERTY(origin, "REMOTE", (int32_t)NativeRdb::AbsPredicates::Origin::REMOTE);
+    SET_NAPI_PROPERTY(origin, "LOCAL", int32_t(NativeRdb::AbsPredicates::Origin::LOCAL));
+    SET_NAPI_PROPERTY(origin, "CLOUD", int32_t(NativeRdb::AbsPredicates::Origin::CLOUD));
+    SET_NAPI_PROPERTY(origin, "REMOTE", int32_t(NativeRdb::AbsPredicates::Origin::REMOTE));
     napi_object_freeze(env, origin);
     return origin;
 }
@@ -135,8 +135,8 @@ static napi_value ExportDistributedType(napi_env env)
     napi_value distributedType = nullptr;
     napi_create_object(env, &distributedType);
 
-    SET_NAPI_PROPERTY(distributedType, "DISTRIBUTED_DEVICE", (int32_t)DistributedTableType::DISTRIBUTED_DEVICE);
-    SET_NAPI_PROPERTY(distributedType, "DISTRIBUTED_CLOUD", (int32_t)DistributedTableType::DISTRIBUTED_CLOUD);
+    SET_NAPI_PROPERTY(distributedType, "DISTRIBUTED_DEVICE", int32_t(DistributedTableType::DISTRIBUTED_DEVICE));
+    SET_NAPI_PROPERTY(distributedType, "DISTRIBUTED_CLOUD", int32_t(DistributedTableType::DISTRIBUTED_CLOUD));
     napi_object_freeze(env, distributedType);
     return distributedType;
 }
@@ -146,8 +146,8 @@ static napi_value ExportChangeType(napi_env env)
     napi_value changeType = nullptr;
     napi_create_object(env, &changeType);
 
-    SET_NAPI_PROPERTY(changeType, "DATA_CHANGE", (int32_t)DistributedRdb::Origin::BASIC_DATA);
-    SET_NAPI_PROPERTY(changeType, "ASSET_CHANGE", (int32_t)DistributedRdb::Origin::ASSET_DATA);
+    SET_NAPI_PROPERTY(changeType, "DATA_CHANGE", int32_t(DistributedRdb::Origin::BASIC_DATA));
+    SET_NAPI_PROPERTY(changeType, "ASSET_CHANGE", int32_t(DistributedRdb::Origin::ASSET_DATA));
     napi_object_freeze(env, changeType);
     return changeType;
 }
@@ -157,12 +157,12 @@ static napi_value ExportAssetStatus(napi_env env)
     napi_value assetStatus = nullptr;
     napi_create_object(env, &assetStatus);
 
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_NORMAL", (int32_t)NativeRdb::AssetValue::STATUS_NORMAL);
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_INSERT", (int32_t)NativeRdb::AssetValue::STATUS_INSERT);
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_UPDATE", (int32_t)NativeRdb::AssetValue::STATUS_UPDATE);
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_DELETE", (int32_t)NativeRdb::AssetValue::STATUS_DELETE);
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_ABNORMAL", (int32_t)NativeRdb::AssetValue::STATUS_ABNORMAL);
-    SET_NAPI_PROPERTY(assetStatus, "ASSET_DOWNLOADING", (int32_t)NativeRdb::AssetValue::STATUS_DOWNLOADING);
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_NORMAL", int32_t(NativeRdb::AssetValue::STATUS_NORMAL));
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_INSERT", int32_t(NativeRdb::AssetValue::STATUS_INSERT));
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_UPDATE", int32_t(NativeRdb::AssetValue::STATUS_UPDATE));
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_DELETE", int32_t(NativeRdb::AssetValue::STATUS_DELETE));
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_ABNORMAL", int32_t(NativeRdb::AssetValue::STATUS_ABNORMAL));
+    SET_NAPI_PROPERTY(assetStatus, "ASSET_DOWNLOADING", int32_t(NativeRdb::AssetValue::STATUS_DOWNLOADING));
     napi_object_freeze(env, assetStatus);
     return assetStatus;
 }
@@ -172,12 +172,12 @@ static napi_value ExportConflictResolution(napi_env env)
     napi_value conflictResolution = nullptr;
     napi_create_object(env, &conflictResolution);
 
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_NONE", (int32_t)ConflictResolution::ON_CONFLICT_NONE);
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_ROLLBACK", (int32_t)ConflictResolution::ON_CONFLICT_ROLLBACK);
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_ABORT", (int32_t)ConflictResolution::ON_CONFLICT_ABORT);
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_FAIL", (int32_t)ConflictResolution::ON_CONFLICT_FAIL);
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_IGNORE", (int32_t)ConflictResolution::ON_CONFLICT_IGNORE);
-    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_REPLACE", (int32_t)ConflictResolution::ON_CONFLICT_REPLACE);
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_NONE", int32_t(ConflictResolution::ON_CONFLICT_NONE));
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_ROLLBACK", int32_t(ConflictResolution::ON_CONFLICT_ROLLBACK));
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_ABORT", int32_t(ConflictResolution::ON_CONFLICT_ABORT));
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_FAIL", int32_t(ConflictResolution::ON_CONFLICT_FAIL));
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_IGNORE", int32_t(ConflictResolution::ON_CONFLICT_IGNORE));
+    SET_NAPI_PROPERTY(conflictResolution, "ON_CONFLICT_REPLACE", int32_t(ConflictResolution::ON_CONFLICT_REPLACE));
 
     napi_object_freeze(env, conflictResolution);
     return conflictResolution;
