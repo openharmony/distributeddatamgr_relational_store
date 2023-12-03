3b3af6b54da785bf1819f11531307141275a23e6
zuojiangjiang <zuojiangjiang@huawei.com>
2023-12-03 15:36:39 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/js/napi/cloud_extension/cloud_sharing_types_util.js
+++ frameworks/js/napi/cloud_extension/cloud_sharing_types_util.js
@@ -15,35 +15,35 @@
 
 let rpc = requireNapi('rpc');
 
-const TAG = "sharingCenterExtension";
+const TAG = 'sharingCenterExtension';
 
 export var cloudExtension;
 !function(e) {
     let t;
     !function(e) {
-        e[e.ConnectShareCenter = 0] = "ConnectShareCenter"
+        e[e.ConnectShareCenter = 0] = 'ConnectShareCenter'
     }(t = e.CloudService_Function || (e.CloudService_Function = {}));
     let a;
     !function(e) {
-        e[e.Share = 0] = "Share";
-        e[e.Unshare = 1] = "Unshare";
-        e[e.Exit = 2] = "Exit";
-        e[e.ChangePrivilege = 3] = "ChangePrivilege";
-        e[e.QueryParticipants = 4] = "QueryParticipants";
-        e[e.QueryParticipantsByInvitation = 5] = "QueryParticipantsByInvitation";
-        e[e.ConfirmInvitation = 6] = "ConfirmInvitation";
-        e[e.ChangeConfirmation = 7] = "ChangeConfirmation"
+        e[e.Share = 0] = 'Share';
+        e[e.Unshare = 1] = 'Unshare';
+        e[e.Exit = 2] = 'Exit';
+        e[e.ChangePrivilege = 3] = 'ChangePrivilege';
+        e[e.QueryParticipants = 4] = 'QueryParticipants';
+        e[e.QueryParticipantsByInvitation = 5] = 'QueryParticipantsByInvitation';
+        e[e.ConfirmInvitation = 6] = 'ConfirmInvitation';
+        e[e.ChangeConfirmation = 7] = 'ChangeConfirmation';
     }(a = e.ShareCenter_Function || (e.ShareCenter_Function = {}));
 
     class r extends rpc.RemoteObject {
         constructor(e, t) {
             super(e);
             this.cloudService = t;
-            console.info(`${TAG} CloudServiceProxy constructor`)
+            console.info(`${TAG} CloudServiceProxy constructor`);
         }
 
         async onRemoteMessageRequest(e, a, r, n) {
-            console.info(`${TAG}, onRemoteRequest called, code = ${e}`);
+            console.info(`${TAG}, onRemoteRequest called, code: ${e}`);
             n.setWaitTime(1e3);
             if (e === t.ConnectShareCenter) {
                 let e = a.readInt();
@@ -52,18 +52,18 @@ export var cloudExtension;
                 let i = await this.cloudService.connectShareCenter(e, n);
                 null == i && console.error(`${TAG}, sharingCenter is null`);
                 r.writeRemoteObject(i);
-                return !0
+                return !0;
             }
             console.error(`${TAG}, invalid request code ${e}`);
-            return !1
+            return !1;
         }
 
         unMarshallingString(e) {
-            let t = "";
+            let t = '';
             e.forEach((e => {
-                t += String.fromCharCode(e)
+                t += String.fromCharCode(e);
             }));
-            return t.toString()
+            return t.toString();
         }
     }
 
@@ -73,11 +73,11 @@ export var cloudExtension;
         constructor(e, t) {
             super(e);
             this.shareCenter = t;
-            console.info(`${TAG} shareCenter constructor`)
+            console.info(`${TAG} shareCenter constructor`);
         }
 
         async onRemoteMessageRequest(e, t, r, n) {
-            console.info(`${TAG}, onRemoteRequest called, code = = ${e}`);
+            console.info(`${TAG}, onRemoteRequest called, code: ${e}`);
             if (void 0 === this.shareCenter) return !1;
             n.setWaitTime(1500);
             switch(e) {
@@ -163,7 +163,7 @@ export var cloudExtension;
                     te.description ? this.marshallingString(r, te.description) : r.writeInt(0);
                     return !0;
                 default:
-                    console.info(`${TAG}, invalid request code`)
+                    console.info(`${TAG}, invalid request code`);
             }
             return !1
         }
@@ -171,9 +171,9 @@ export var cloudExtension;
         unMarshallingParticipants(e) {
             let t = [];
             let a = e.readInt();
-            console.info(`${TAG}, query fields length = ${a}`);
+            console.info(`${TAG}, query fields length: ${a}`);
             for (let r = 0; r < a; r++) t.push(this.unMarshallingParticipant(e));
-            return t
+            return t;
         }
 
         unMarshallingParticipant(e) {
@@ -186,7 +186,7 @@ export var cloudExtension;
                 privilege: this.unMarshallingPrivilege(e),
                 attachInfo: this.unMarshallingString(e.readRawData(e.readInt()))
             };
-            return a
+            return a;
         }
 
         unMarshallingPrivilege(e) {
@@ -198,7 +198,7 @@ export var cloudExtension;
                 deletable: e.readBoolean(),
                 shareable: e.readBoolean()
             };
-            return t
+            return t;
         }
 
         marshallingResultsArray(e, t) {
@@ -207,7 +207,7 @@ export var cloudExtension;
             if (t.value) {
                 e.writeInt(t.value.length);
                 t.value.forEach((t => {
-                    this.marshallingParticipant(e, t)
+                    this.marshallingParticipant(e, t);
                 }))
             }
         }
@@ -219,7 +219,7 @@ export var cloudExtension;
                 e.writeInt(t.value.length);
                 t.value.forEach((t => {
                     e.writeInt(t.code);
-                    t.description ? this.marshallingString(e, t.description) : e.writeInt(0)
+                    t.description ? this.marshallingString(e, t.description) : e.writeInt(0);
                 }))
             }
         }
@@ -229,7 +229,7 @@ export var cloudExtension;
             e.writeInt(t.role);
             e.writeInt(t.state);
             t.privilege && this.marshallingPrivilege(e, t.privilege);
-            t.attachInfo ? this.marshallingString(e, t.attachInfo) : e.writeInt(0)
+            t.attachInfo ? this.marshallingString(e, t.attachInfo) : e.writeInt(0);
         }
 
         marshallingPrivilege(e, t) {
@@ -237,15 +237,15 @@ export var cloudExtension;
             e.writeBoolean(t.readable);
             e.writeBoolean(t.creatable);
             e.writeBoolean(t.deletable);
-            e.writeBoolean(t.shareable)
+            e.writeBoolean(t.shareable);
         }
 
         unMarshallingString(e) {
-            let t = "";
+            let t = '';
             e.forEach((e => {
-                t += String.fromCharCode(e)
+                t += String.fromCharCode(e);
             }));
-            return t.toString()
+            return t.toString();
         }
 
         marshallingString(e, t) {
@@ -255,16 +255,16 @@ export var cloudExtension;
             let n = t.length;
             e.writeInt(n);
             for (let e = 0; e < n; e++) a.push(r.charCodeAt(e));
-            e.writeRawData(a, n)
+            e.writeRawData(a, n);
         }
     }
 
     e.ShareCenterProxy = n;
     e.createCloudServiceStub = async function (e) {
-        return new r("CloudServiceProxy", e)
+        return new r('CloudServiceProxy', e);
     };
     e.createShareServiceStub = async function (e) {
-        return new n("ShareCenterProxy", e)
+        return new n('ShareCenterProxy', e);
     }
 }(cloudExtension || (cloudExtension = {}));
 
