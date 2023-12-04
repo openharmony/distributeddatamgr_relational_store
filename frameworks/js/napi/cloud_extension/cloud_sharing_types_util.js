f794b0b2cdcb5482215c589d3a32295921c9f002
zuojiangjiang <zuojiangjiang@huawei.com>
2023-12-04 16:17:16 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/js/napi/cloud_extension/cloud_sharing_types_util.js
+++ frameworks/js/napi/cloud_extension/cloud_sharing_types_util.js
@@ -1,166 +1,132 @@
-/*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 let rpc = requireNapi('rpc');
 
-const TAG = 'sharingCenterExtension';
+const TAG = "sharingCenterExtension";
 
 export var cloudExtension;
 !function(e) {
     let t;
     !function(e) {
-        e[e.ConnectShareCenter = 0] = 'ConnectShareCenter'
+        e[e.ConnectShareCenter = 0] = "ConnectShareCenter"
     }(t = e.CloudService_Function || (e.CloudService_Function = {}));
-    let a;
+    let r;
     !function(e) {
-        e[e.Share = 0] = 'Share';
-        e[e.Unshare = 1] = 'Unshare';
-        e[e.Exit = 2] = 'Exit';
-        e[e.ChangePrivilege = 3] = 'ChangePrivilege';
-        e[e.QueryParticipants = 4] = 'QueryParticipants';
-        e[e.QueryParticipantsByInvitation = 5] = 'QueryParticipantsByInvitation';
-        e[e.ConfirmInvitation = 6] = 'ConfirmInvitation';
-        e[e.ChangeConfirmation = 7] = 'ChangeConfirmation';
-    }(a = e.ShareCenter_Function || (e.ShareCenter_Function = {}));
+        e[e.Share = 0] = "Share";
+        e[e.Unshare = 1] = "Unshare";
+        e[e.Exit = 2] = "Exit";
+        e[e.ChangePrivilege = 3] = "ChangePrivilege";
+        e[e.QueryParticipants = 4] = "QueryParticipants";
+        e[e.QueryParticipantsByInvitation = 5] = "QueryParticipantsByInvitation";
+        e[e.ConfirmInvitation = 6] = "ConfirmInvitation";
+        e[e.ChangeConfirmation = 7] = "ChangeConfirmation";
+    }(r = e.ShareCenter_Function || (e.ShareCenter_Function = {}));
 
-    class r extends rpc.RemoteObject {
+    class n extends rpc.RemoteObject {
         constructor(e, t) {
             super(e);
             this.cloudService = t;
             console.info(`${TAG} CloudServiceProxy constructor`);
         }
 
-        async onRemoteMessageRequest(e, a, r, n) {
+        async onRemoteMessageRequest(e, r, n, i) {
             console.info(`${TAG}, onRemoteRequest called, code: ${e}`);
-            n.setWaitTime(1e3);
+            i.setWaitTime(1e3);
             if (e === t.ConnectShareCenter) {
                 let e = a.readInt();
-                let t = a.readInt();
-                let n = this.unMarshallingString(a.readRawData(t));
-                let i = await this.cloudService.connectShareCenter(e, n);
-                null == i && console.error(`${TAG}, sharingCenter is null`);
-                r.writeRemoteObject(i);
+                let t = a.readString();
+                let i = await this.cloudService.connectShareCenter(e, t);
+                if (null == i) {
+                    console.error(`${TAG}, sharingCenter is null`);
+                    return !1;
+                }
+                n.writeRemoteObject(i);
                 return !0;
             }
             console.error(`${TAG}, invalid request code ${e}`);
             return !1;
         }
-
-        unMarshallingString(e) {
-            let t = '';
-            e.forEach((e => {
-                t += String.fromCharCode(e);
-            }));
-            return t.toString();
-        }
     }
 
-    e.CloudServiceProxy = r;
+    e.CloudServiceProxy = n;
 
-    class n extends rpc.RemoteObject {
+    class i extends rpc.RemoteObject {
         constructor(e, t) {
             super(e);
             this.shareCenter = t;
             console.info(`${TAG} shareCenter constructor`);
         }
 
-        async onRemoteMessageRequest(e, t, r, n) {
+        async onRemoteMessageRequest(e, t, n, i) {
             console.info(`${TAG}, onRemoteRequest called, code: ${e}`);
-            if (void 0 === this.shareCenter) return !1;
-            n.setWaitTime(1500);
+            if (null == this.shareCenter) {
+                console.info(`${TAG} shareCenter undefined`);
+                return !1;
+            }
+            i.setWaitTime(1500);
             switch(e) {
-                case a.Share:
+                case r.Share:
                     let e = t.readInt();
-                    let n = t.readInt();
-                    let i = this.unMarshallingString(t.readRawData(n));
-                    let l = t.readInt();
-                    let s = this.unMarshallingString(t.readRawData(l));
-                    let o = this.unMarshallingParticipants(t);
-                    let h = await this.shareCenter.share(e, i, s, o);
-                    this.marshallingResults(r, h);
+                    let i = t.readString();
+                    let a = t.readString();
+                    let l = this.unMarshallingParticipants(t);
+                    let o = await this.shareCenter.share(e, i, a, l);
+                    this.marshallingResults(n, o);
                     return !0;
-                case a.Unshare:
-                    let c = t.readInt();
-                    let u = t.readInt();
-                    let g = this.unMarshallingString(t.readRawData(u));
-                    let d = t.readInt();
-                    let I = this.unMarshallingString(t.readRawData(d));
-                    let w = this.unMarshallingParticipants(t);
-                    let S = await this.shareCenter.unshare(c, g, I, w);
-                    this.marshallingResults(r, S);
+                case r.Unshare:
+                    let s = t.readInt();
+                    let c = t.readString();
+                    let h = t.readString();
+                    let d = this.unMarshallingParticipants(t);
+                    let u = await this.shareCenter.unshare(s, c, h, d);
+                    this.marshallingResults(n, u);
                     return !0;
-                case a.Exit:
-                    let C = t.readInt();
-                    let m = t.readInt();
-                    let f = this.unMarshallingString(t.readRawData(m));
-                    let p = t.readInt();
-                    let v = this.unMarshallingString(t.readRawData(p));
-                    let R = await this.shareCenter.exit(C, f, v);
-                    r.writeInt(R.code);
-                    R.description ? this.marshallingString(r, R.description) : r.writeInt(0);
+                case r.Exit:
+                    let g = t.readInt();
+                    let S = t.readString();
+                    let w = t.readString();
+                    let C = await this.shareCenter.exit(g, S, w);
+                    r.writeInt(C.code);
+                    C.description ? n.writeString(C.description) : n.writeString("");
                     return !0;
-                case a.ChangePrivilege:
-                    let M = t.readInt();
+                case r.ChangePrivilege:
+                    let v = t.readInt();
+                    let p = t.readString();
+                    let I = t.readString();
+                    let f = this.unMarshallingParticipants(t);
+                    let m = await this.shareCenter.changePrivilege(v, p, I, f);
+                    this.marshallingResults(n, m);
+                    return !0;
+                case r.QueryParticipants:
                     let P = t.readInt();
-                    let y = this.unMarshallingString(t.readRawData(P));
-                    let D = t.readInt();
-                    let x = this.unMarshallingString(t.readRawData(D));
-                    let b = this.unMarshallingParticipants(t);
-                    let $ = await this.shareCenter.changePrivilege(M, y, x, b);
-                    this.marshallingResults(r, $);
+                    let y = t.readString();
+                    let B = t.readString();
+                    let R = await this.shareCenter.queryParticipants(P, y, B);
+                    this.marshallingResultsArray(n, R);
                     return !0;
-                case a.QueryParticipants:
-                    let B = t.readInt();
-                    let A = t.readInt();
-                    let E = this.unMarshallingString(t.readRawData(A));
-                    let T = t.readInt();
-                    let q = this.unMarshallingString(t.readRawData(T));
-                    let j = await this.shareCenter.queryParticipants(B, E, q);
-                    this.marshallingResultsArray(r, j);
+                case r.QueryParticipantsByInvitation:
+                    let x = t.readInt();
+                    let A = t.readString();
+                    let b = t.readString();
+                    let T = await this.shareCenter.queryParticipantsByInvitation(x, A, b);
+                    this.marshallingResultsArray(n, T);
                     return !0;
-                case a.QueryParticipantsByInvitation:
-                    let G = t.readInt();
+                case r.ConfirmInvitation:
+                    let $ = t.readInt();
+                    let E = t.readString();
+                    let G = t.readString();
+                    let M = t.readInt();
+                    let q = await this.shareCenter.confirmInvitation($, E, G, M);
+                    n.writeInt(q.code);
+                    q.description ? n.writeString(q.description) : n.writeString("");
+                    q.value ? n.writeString(q.value) : n.writeString("");
+                    return !0;
+                case r.ChangeConfirmation:
                     let Q = t.readInt();
-                    let F = this.unMarshallingString(t.readRawData(Q));
-                    let O = t.readInt();
-                    let _ = this.unMarshallingString(t.readRawData(O));
-                    let z = await this.shareCenter.queryParticipantsByInvitation(G, F, _);
-                    this.marshallingResultsArray(r, z);
-                    return !0;
-                case a.ConfirmInvitation:
-                    let U = t.readInt();
-                    let W = t.readInt();
-                    let J = this.unMarshallingString(t.readRawData(W));
-                    let N = t.readInt();
-                    let k = this.unMarshallingString(t.readRawData(N));
-                    let H = t.readInt();
-                    let K = await this.shareCenter.confirmInvitation(U, J, k, H);
-                    r.writeInt(K.code);
-                    K.description ? this.marshallingString(r, K.description) : r.writeInt(0);
-                    K.value ? this.marshallingString(r, K.value) : r.writeInt(0);
-                    return !0;
-                case a.ChangeConfirmation:
-                    let L = t.readInt();
-                    let V = t.readInt();
-                    let X = this.unMarshallingString(t.readRawData(V));
-                    let Y = t.readInt();
-                    let Z = this.unMarshallingString(t.readRawData(Y));
-                    let ee = t.readInt();
-                    let te = await this.shareCenter.changeConfirmation(L, X, Z, ee);
-                    r.writeInt(te.code);
-                    te.description ? this.marshallingString(r, te.description) : r.writeInt(0);
+                    let _ = t.readString();
+                    let F = t.readString();
+                    let j = t.readInt();
+                    let O = await this.shareCenter.changeConfirmation(Q, _, F, j);
+                    n.writeInt(O.code);
+                    O.description ? n.writeString(O.description) : n.writeString("");
                     return !0;
                 default:
                     console.info(`${TAG}, invalid request code`);
@@ -170,23 +136,25 @@ export var cloudExtension;
 
         unMarshallingParticipants(e) {
             let t = [];
-            let a = e.readInt();
-            console.info(`${TAG}, query fields length: ${a}`);
-            for (let r = 0; r < a; r++) t.push(this.unMarshallingParticipant(e));
+            let r = e.readInt();
+            console.info(`${TAG}, array length: ${r}`);
+            for (let n = 0; n < r; n++) t.push(this.unMarshallingParticipant(e));
             return t;
         }
 
         unMarshallingParticipant(e) {
-            let t = e.readInt();
-            let a;
-            a = {
-                identity: this.unMarshallingString(e.readRawData(t)),
-                role: e.readInt(),
-                state: e.readInt(),
+            let t = e.readString();
+            let r;
+            let n = e.readInt();
+            let i = e.readInt();
+            r = {
+                identity: t,
+                role: -1 == n ? void 0 : n,
+                state: -1 == i ? void 0 : n,
                 privilege: this.unMarshallingPrivilege(e),
-                attachInfo: this.unMarshallingString(e.readRawData(e.readInt()))
+                attachInfo: e.readString()
             };
-            return a;
+            return r;
         }
 
         unMarshallingPrivilege(e) {
@@ -203,7 +171,7 @@ export var cloudExtension;
 
         marshallingResultsArray(e, t) {
             e.writeInt(t.code);
-            t.description ? this.marshallingString(e, t.description) : e.writeInt(0);
+            t.description ? e.writeString(t.description) : e.writeString("");
             if (t.value) {
                 e.writeInt(t.value.length);
                 t.value.forEach((t => {
@@ -214,22 +182,28 @@ export var cloudExtension;
 
         marshallingResults(e, t) {
             e.writeInt(t.code);
-            t.description ? this.marshallingString(e, t.description) : e.writeInt(0);
+            t.description ? e.writeString(t.description) : e.writeString("");
             if (t.value) {
                 e.writeInt(t.value.length);
                 t.value.forEach((t => {
                     e.writeInt(t.code);
-                    t.description ? this.marshallingString(e, t.description) : e.writeInt(0);
+                    t.description ? e.writeString(t.description) : e.writeString("");
                 }))
             }
         }
 
         marshallingParticipant(e, t) {
-            this.marshallingString(e, t.identity);
-            e.writeInt(t.role);
-            e.writeInt(t.state);
-            t.privilege && this.marshallingPrivilege(e, t.privilege);
-            t.attachInfo ? this.marshallingString(e, t.attachInfo) : e.writeInt(0);
+            e.writeString(t.identity);
+            void 0 !== t.role? e.writeInt(t.role) : e.writeInt(-1);
+            void 0 !== t.state? e.writeInt(t.state) : e.writeInt(-1);
+            if (t.privilege) this.marshallingPrivilege(e, t.privilege); else {
+                e.writeBoolean(!1);
+                e.writeBoolean(!1);
+                e.writeBoolean(!1);
+                e.writeBoolean(!1);
+                e.writeBoolean(!1)
+            }
+            t.attachInfo ? e.writeString(t.attachInfo) : e.writeString("");
         }
 
         marshallingPrivilege(e, t) {
@@ -239,32 +213,14 @@ export var cloudExtension;
             e.writeBoolean(t.deletable);
             e.writeBoolean(t.shareable);
         }
-
-        unMarshallingString(e) {
-            let t = '';
-            e.forEach((e => {
-                t += String.fromCharCode(e);
-            }));
-            return t.toString();
-        }
-
-        marshallingString(e, t) {
-            let a = [];
-            let r;
-            r = t;
-            let n = t.length;
-            e.writeInt(n);
-            for (let e = 0; e < n; e++) a.push(r.charCodeAt(e));
-            e.writeRawData(a, n);
-        }
     }
 
-    e.ShareCenterProxy = n;
+    e.ShareCenterProxy = i;
     e.createCloudServiceStub = async function (e) {
-        return new r('CloudServiceProxy', e);
+        return new n('CloudServiceProxy', e);
     };
     e.createShareServiceStub = async function (e) {
-        return new n('ShareCenterProxy', e);
+        return new i('ShareCenterProxy', e);
     }
 }(cloudExtension || (cloudExtension = {}));
 
