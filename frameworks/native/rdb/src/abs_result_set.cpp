88eb4743c4f0930f70f9ba520e82efe372f31ae3
yangliu <yangliu178@huawei.com>
2024-01-27 14:41:01 +0800

    update
    Signed-off-by: yangliu <yangliu178@huawei.com>

--- frameworks/native/rdb/src/abs_result_set.cpp
+++ frameworks/native/rdb/src/abs_result_set.cpp
@@ -346,7 +346,7 @@ int AbsResultSet::GetColumnIndex(const std::string &columnName, int &columnIndex
         columnIndex++;
     }
     columnIndex = -1;
-    LOG_ERROR("GetColumnIndex failed %{public}s", columnName.c_str());
+    LOG_ERROR("GetColumnIndex failed, columnName is: %{public}s", columnName.c_str());
     return E_ERROR;
 }
 
