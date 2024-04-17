24ba76cc06202dd3317366da3e76a5456a0f595b
zhangqing <zhangqing50@h-partners.com>
2024-04-17 17:27:27 +0800

    arkui-x update master OH to weekly_20240408 and OH fix build bug
    Signed-off-by: zhangqing <zhangqing50@h-partners.com>
    Change-Id: I7b675738e7a96e404889d03c17499dcfe3450a0f

--- frameworks/js/napi/relationalstore/src/napi_rdb_store.cpp
+++ frameworks/js/napi/relationalstore/src/napi_rdb_store.cpp
@@ -117,6 +117,7 @@ RdbStoreProxy::~RdbStoreProxy()
     if (rdbStore_ == nullptr) {
         return;
     }
+#if !defined(WINDOWS_PLATFORM) && !defined(MAC_PLATFORM) && !defined(ANDROID_PLATFORM) && !defined(IOS_PLATFORM)
     for (int32_t mode = DistributedRdb::REMOTE; mode < DistributedRdb::LOCAL; mode++) {
         for (auto &obs : observers_[mode]) {
             if (obs == nullptr) {
@@ -144,6 +145,7 @@ RdbStoreProxy::~RdbStoreProxy()
     for (const auto &obs : syncObservers_) {
         rdbStore_->UnregisterAutoSyncCallback(obs);
     }
+#endif
 }
 
 bool RdbStoreProxy::IsSystemAppCalled()
