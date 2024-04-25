289d30c622a582d5a0283ab9f397ba4123b4cf01
marui <marui83@huawei.com>
2024-04-25 11:55:40 +0800

    4.25 modify warning Signed-off-by: marui <marui83@huawei.com>

--- frameworks/native/rdb/src/step_result_set.cpp
+++ frameworks/native/rdb/src/step_result_set.cpp
@@ -534,7 +534,7 @@ int StepResultSet::GetSize(int columnIndex, size_t &size)
 
 int StepResultSet::IsColumnNull(int columnIndex, bool &isNull)
 {
-    ColumnType columnType;
+    ColumnType columnType = ColumnType::TYPE_NULL;
     int errCode = GetColumnType(columnIndex, columnType);
     if (errCode != E_OK) {
         LOG_ERROR("ret is %{public}d", errCode);
