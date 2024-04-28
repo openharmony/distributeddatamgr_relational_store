3c30a61afd3a3fcf0539fceaa07f648a8a9cca2d
zuojiangjiang <zuojiangjiang@huawei.com>
2024-04-28 15:36:24 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/js/napi/common/src/js_uv_queue.cpp
+++ frameworks/js/napi/common/src/js_uv_queue.cpp
@@ -27,7 +27,7 @@ UvQueue::UvQueue(napi_env env) : env_(env)
     if (env != nullptr) {
         napi_get_uv_event_loop(env, &loop_);
     }
-    handler_ = std::make_shared<AppExecFwk::EventHandler>(AppExecFwk::EventRunner::GetMainEventRunner());
+    handler_ = AppExecFwk::EventHandler::Current();
 }
 
 UvQueue::~UvQueue()
