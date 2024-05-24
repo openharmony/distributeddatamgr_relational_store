2912acce783eebbd2dbcea89bb14b02fd83940c5
zuojiangjiang <zuojiangjiang@huawei.com>
2024-05-24 21:25:09 +0800

    update Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/native/rdb/src/sqlite_connection.cpp
+++ frameworks/native/rdb/src/sqlite_connection.cpp
@@ -150,7 +150,8 @@ void SqliteConnection::ReadFile2Buffer(const char* fileName)
         (void)fclose(file);
         return;
     }
-    for (int i = 0; i < BUFFER_LEN; i += 4) {
+    const int num = 4;
+    for (int i = 0; i < BUFFER_LEN; i += num) {
         LOG_WARN("line%{public}d: %{public}" PRIx64 "%{public}" PRIx64 "%{public}" PRIx64 "%{public}" PRIx64,
             i >> 2, buffer[i], buffer[i + 1], buffer[i + 2], buffer[i + 3]);
     }
