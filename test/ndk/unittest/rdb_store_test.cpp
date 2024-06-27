6b0bf43cbe8286127e003d646d7293ae2d875e00
luyangyang <1049603119@qq.com>
2024-06-27 19:24:50 +0800

    检视意见修复 Signed-off-by: luyangyang <1049603119@qq.com>

--- test/ndk/unittest/rdb_store_test.cpp
+++ test/ndk/unittest/rdb_store_test.cpp
@@ -1510,7 +1510,7 @@ HWTEST_F(RdbNativeStoreTest, RDB_Native_store_test_034, TestSize.Level1)
     OH_Rdb_Store *rdbStore;
     rdbStore = OH_Rdb_GetOrOpen(&config, &errCode);
     EXPECT_EQ(rdbStore, nullptr);
-    if (config->securityLevel < OH_Rdb_SecurityLevel::S1 || config->securityLevel > OH_Rdb_SecurityLevel::S4) {
+    if (config.securityLevel < OH_Rdb_SecurityLevel::S1 || config.securityLevel > OH_Rdb_SecurityLevel::S4) {
         EXPECT_EQ(errCode, OH_Rdb_ErrCode::RDB_E_INVALID_ARGS);
     } else {
         EXPECT_NE(errCode, OH_Rdb_ErrCode::RDB_E_INVALID_ARGS);
