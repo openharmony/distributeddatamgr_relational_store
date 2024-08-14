94a143ebeea3d34c73ed27f7ebaac3c9b2e18751
张琼洁 <zhangqiongjie4@huawei.com>
2024-08-14 03:04:17 +0000

    update frameworks/native/rdb/src/rdb_store_impl.cpp.
    Signed-off-by: 张琼洁 <zhangqiongjie4@huawei.com>

--- frameworks/native/rdb/src/rdb_store_impl.cpp
+++ frameworks/native/rdb/src/rdb_store_impl.cpp
@@ -113,14 +113,6 @@ int RdbStoreImpl::InnerOpen()
 }
 
 #if !defined(WINDOWS_PLATFORM) && !defined(MAC_PLATFORM) && !defined(ANDROID_PLATFORM) && !defined(IOS_PLATFORM)
-std::string RdbStoreImpl::GetSecManagerName(const RdbStoreConfig &config)
-{
-    auto name = config.GetBundleName();
-    if (name.empty()) {
-        return std::string(config.GetPath()).substr(0, config.GetPath().rfind("/") + 1);
-    }
-    return name;
-}
 
 void RdbStoreImpl::AfterOpen(const RdbStoreConfig &config)
 {
