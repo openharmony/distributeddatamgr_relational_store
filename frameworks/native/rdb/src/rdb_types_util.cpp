dde94c780bf12bffb8403b4e785457055fc29906
Sven Wang <wanghancai@huawei.com>
2024-09-18 10:19:19 +0800

    Update
    Signed-off-by: Sven Wang <wanghancai@huawei.com>

--- frameworks/native/rdb/src/rdb_types_util.cpp
+++ frameworks/native/rdb/src/rdb_types_util.cpp
@@ -20,14 +20,14 @@ bool Marshalling(const SyncerParam &input, MessageParcel &data)
 {
     return ITypesUtil::Marshal(data, input.bundleName_, input.hapName_, input.storeName_, input.area_,
         input.level_, input.type_, input.isEncrypt_, input.password_, input.customDir_, input.isAutoClean_,
-        input.isSearchable_, input.haMode_);
+        input.isSearchable_, input.haMode_, input.infos_);
 }
 template<>
 bool Unmarshalling(SyncerParam &output, MessageParcel &data)
 {
     return ITypesUtil::Unmarshal(data, output.bundleName_, output.hapName_, output.storeName_, output.area_,
         output.level_, output.type_, output.isEncrypt_, output.password_, output.customDir_, output.isAutoClean_,
-        output.isSearchable_, output.haMode_);
+        output.isSearchable_, output.haMode_, output.infos_);
 }
 
 template<>
