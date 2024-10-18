d6499dda663fc032369dc8de89bfb173cb31c46a
Sven Wang <wanghancai@huawei.com>
2024-10-18 16:38:55 +0800

    Update
    Signed-off-by: Sven Wang <wanghancai@huawei.com>

--- frameworks/native/rdb/src/transaction.cpp
+++ frameworks/native/rdb/src/transaction.cpp
@@ -14,67 +14,19 @@
  */
 #include "transaction.h"
 
-#include "connection.h"
-#include "rdb_errno.h"
-#include "trans_db.h"
 namespace OHOS::NativeRdb {
-class TransactionImpl : public Transaction {
-public:
-    TransactionImpl(std::shared_ptr<Connection>, const std::string &name);
-    int32_t Begin(int32_t type);
-    int32_t Commit() override;
-    int32_t Rollback() override;
-    std::pair<int32_t, std::shared_ptr<RdbStore>> GetTransDB() override;
-    int32_t Close() override;
-
-private:
-    std::string name_;
-    std::shared_ptr<Connection> conn_;
-};
-
-std::pair<int32_t, std::shared_ptr<Transaction>> Transaction::Create(int32_t type, std::shared_ptr<Connection> conn,
-    const std::string &name)
+std::pair<int32_t, std::shared_ptr<Transaction>> Transaction::Create(
+    int32_t type, std::shared_ptr<Connection> connection, const std::string &name)
 {
-    if (type < DEFERRED || type >= TRANS_BUTT) {
-        return { E_INVALID_ARGS, nullptr };
+    if (creator_ != nullptr) {
+        return creator_(type, std::move(connection), name);
     }
-    auto transImpl = std::make_shared<TransactionImpl>(conn, name);
-    auto errCode = transImpl->Begin(type);
-    if (errCode != E_OK) {
-        return { errCode, nullptr };
-    }
-    return { E_OK, transImpl };
+    return { E_ERROR, nullptr };
 }
 
-TransactionImpl::TransactionImpl(std::shared_ptr<Connection> conn, const std::string &name)
-    : name_(name), conn_(std::move(conn))
+int32_t Transaction::RegisterCreator(Creator creator)
 {
-}
-int32_t TransactionImpl::Begin(int32_t type)
-{
-    if (type < DEFERRED || type >= TRANS_BUTT) {
-        return E_INVALID_ARGS;
-    }
+    creator_ = std::move(creator);
     return E_OK;
 }
-int32_t TransactionImpl::Commit()
-{
-    return E_OK;
 }
-int32_t TransactionImpl::Rollback()
-{
-    return E_OK;
-}
-
-std::pair<int32_t, std::shared_ptr<RdbStore>> TransactionImpl::GetTransDB()
-{
-    auto store = std::make_shared<TransDB>(conn_, name_);
-    return { E_OK, store };
-}
-
-int32_t TransactionImpl::Close()
-{
-    conn_ = nullptr;
-    return 0;
-}
-} // namespace OHOS::NativeRdb
