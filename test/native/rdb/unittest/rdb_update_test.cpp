4c1111d56b7addf0e2bcd47830ea3c689b69ef5c
Anvette <anwentao@huawei.com>
2025-03-05 21:27:25 +0800

    add fix
    Signed-off-by: Anvette <anwentao@huawei.com>

--- test/native/rdb/unittest/rdb_update_test.cpp
+++ test/native/rdb/unittest/rdb_update_test.cpp
@@ -845,7 +845,7 @@ HWTEST_P(RdbStoreUpdateTest, OverLimitWithUpdate_001, TestSize.Level1)
 {
     std::shared_ptr<RdbStore> store = *GetParam();
     auto [code, maxPageCount] = store->Execute("PRAGMA max_page_count;");
-    auto recover = std::shared_ptr("recover", [defPageCount = maxPageCount, store](const char *) {
+    auto recover = std::shared_ptr<const char>("recover", [defPageCount = maxPageCount, store](const char *) {
         store->Execute("PRAGMA max_page_count = " + static_cast<std::string>(defPageCount) + ";");
     });
     std::tie(code, maxPageCount) = store->Execute("PRAGMA max_page_count = 256;");
