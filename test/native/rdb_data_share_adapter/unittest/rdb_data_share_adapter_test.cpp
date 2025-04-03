d8474764f11a2ccbd19d55ad45ce0c7e9ea9350b
guochao <guochao76@huawei.com>
2025-04-03 17:27:52 +0800

    add代码覆盖率 Signed-off-by: guochao <guochao76@huawei.com>

--- test/native/rdb_data_share_adapter/unittest/rdb_data_share_adapter_test.cpp
+++ test/native/rdb_data_share_adapter/unittest/rdb_data_share_adapter_test.cpp
@@ -13,6 +13,7 @@
  * limitations under the License.
  */
 #define LOG_TAG "RdbDataShareAdapterTest"
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <string>
@@ -30,27 +31,81 @@ using namespace OHOS::NativeRdb;
 using namespace OHOS::DataShare;
 using namespace OHOS::RdbDataShareAdapter;
 
+class MockRdbPredicates : public RdbPredicates {
+public:
+    explicit MockRdbPredicates(const std::string &table) : RdbPredicates(table)
+    {
+    }
+    MOCK_METHOD(AbsRdbPredicates *, EqualTo, (const std::string &field, const ValueObject &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, NotEqualTo, (const std::string &column, const OHOS::NativeRdb::ValueObject &value),
+        (override));
+    MOCK_METHOD(AbsRdbPredicates *, GreaterThan,
+        (const std::string &column, const OHOS::NativeRdb::ValueObject &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, LessThan, (const std::string &field, const ValueObject &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, GreaterThanOrEqualTo,
+        (const std::string &column, const OHOS::NativeRdb::ValueObject &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, LessThanOrEqualTo,
+        (const std::string &column, const OHOS::NativeRdb::ValueObject &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, And, (), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Or, (), (override));
+    MOCK_METHOD(AbsRdbPredicates *, IsNull, (const std::string &column), (override));
+    MOCK_METHOD(AbsRdbPredicates *, IsNotNull, (const std::string &column), (override));
+    MOCK_METHOD(AbsRdbPredicates *, In, (const std::string &field, const std::vector<ValueObject> &values), (override));
+    MOCK_METHOD(
+        AbsRdbPredicates *, NotIn, (const std::string &field, const std::vector<ValueObject> &values), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Like, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, NotLike, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, OrderByAsc, (const std::string &field), (override));
+    MOCK_METHOD(AbsRdbPredicates *, OrderByDesc, (const std::string &field), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Offset, (const int offset), (override));
+    MOCK_METHOD(AbsRdbPredicates *, BeginsWith, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, EndsWith, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, GroupBy, (const std::vector<std::string> &fields), (override));
+    MOCK_METHOD(AbsRdbPredicates *, IndexedBy, (const std::string &indexName), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Contains, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, NotContains, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Glob, (const std::string &field, const std::string &value), (override));
+    MOCK_METHOD(AbsRdbPredicates *, Between,
+        (const std::string &field, const ValueObject &low, const ValueObject &high), (override));
+    MOCK_METHOD(AbsRdbPredicates *, NotBetween,
+        (const std::string &field, const ValueObject &low, const ValueObject &high), (override));
+};
 class RdbDataShareAdapterTest : public testing::Test {
 public:
     static void SetUpTestCase(void);
-
     static void TearDownTestCase(void);
-
     void SetUp();
-
     void TearDown();
-
     void GenerateDefaultTable();
-
     void GenerateDefaultEmptyTable();
-
     int ResultSize(std::shared_ptr<ResultSet> resultSet);
-
+    OperationItem CreateOperationItem(int operation, const std::vector<SingleValue::Type> &singleParams,
+        const std::vector<MutliValue::Type> &multiParams);
+    void LessThanTest(MockRdbPredicates &predicates);
+    void GreaterThanOrEqualTo(MockRdbPredicates &predicates);
+    void LessThanOrEqualTo(MockRdbPredicates &predicates);
+    void IsNull(MockRdbPredicates &predicates);
+    void IsNotNull(MockRdbPredicates &predicates);
+    void In(MockRdbPredicates &predicates);
+    void NotIn(MockRdbPredicates &predicates);
+    void Like(MockRdbPredicates &predicates);
+    void NotLike(MockRdbPredicates &predicates);
+    void OrderByAsc(MockRdbPredicates &predicates);
+    void OrderByDesc(MockRdbPredicates &predicates);
+    void Offset(MockRdbPredicates &predicates);
+    void BeginsWith(MockRdbPredicates &predicates);
+    void EndsWith(MockRdbPredicates &predicates);
+    void GroupBy(MockRdbPredicates &predicates);
+    void IndexedBy(MockRdbPredicates &predicates);
+    void Contains(MockRdbPredicates &predicates);
+    void NotContains(MockRdbPredicates &predicates);
+    void Glob(MockRdbPredicates &predicates);
+    void Between(MockRdbPredicates &predicates);
+    void NotBetween(MockRdbPredicates &predicates);
     static const std::string DATABASE_NAME;
     static std::shared_ptr<RdbStore> store;
     static const std::string RDB_ADAPTER_TEST_PATH;
 };
-
 const std::string RdbDataShareAdapterTest::RDB_ADAPTER_TEST_PATH = "/data/test/";
 const std::string RdbDataShareAdapterTest::DATABASE_NAME = RDB_ADAPTER_TEST_PATH + "rdbDataShareAdapter_test.db";
 std::shared_ptr<RdbStore> RdbDataShareAdapterTest::store = nullptr;
@@ -122,10 +177,11 @@ void RdbDataShareAdapterTest::GenerateDefaultTable()
                                  });
 
     /* insert third entry data */
-    store->ExecuteSql(insertSql, std::vector<ValueObject>{
-        ValueObject(std::string("hello world")), ValueObject((int)3), ValueObject((double)1.8),
-        ValueObject(std::vector<uint8_t>{ 4, 5, 6 }) // set int value 3, double 1.8
-    });
+    store->ExecuteSql(
+        insertSql, std::vector<ValueObject>{
+                       ValueObject(std::string("hello world")), ValueObject((int)3), ValueObject((double)1.8),
+                       ValueObject(std::vector<uint8_t>{ 4, 5, 6 }) // set int value 3, double 1.8
+                   });
 
     /* insert four entry data */
     store->ExecuteSql(insertSql, std::vector<ValueObject>{
@@ -151,6 +207,16 @@ int RdbDataShareAdapterTest::ResultSize(std::shared_ptr<ResultSet> resultSet)
         count++;
     }
     return count;
+}
+
+OperationItem RdbDataShareAdapterTest::CreateOperationItem(int operation,
+    const std::vector<SingleValue::Type> &singleParams = {}, const std::vector<MutliValue::Type> &multiParams = {})
+{
+    OperationItem item;
+    item.operation = operation;
+    item.singleParams = singleParams;
+    item.multiParams = multiParams;
+    return item;
 }
 
 /* *
@@ -521,4 +587,233 @@ HWTEST_F(RdbDataShareAdapterTest, Rdb_DataShare_Adapter_009, TestSize.Level1)
     RdbUtils::LessThan(item, predicates);
     allPerson = store->Query(predicates, columns);
     EXPECT_EQ(2, ResultSize(allPerson));
+}
+
+void RdbDataShareAdapterTest::LessThanTest(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, LessThan(testing::_, testing::_)).Times(1);
+    auto item = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::LessThan(item, predicates);
+}
+
+void RdbDataShareAdapterTest::GreaterThanOrEqualTo(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, GreaterThanOrEqualTo(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, { "John" });
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::GreaterThanOrEqualTo(item1, predicates);
+    RdbUtils::GreaterThanOrEqualTo(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::LessThanOrEqualTo(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, LessThanOrEqualTo(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, { "John" });
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::LessThanOrEqualTo(item1, predicates);
+    RdbUtils::LessThanOrEqualTo(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::IsNull(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, IsNull(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::IsNull(item1, predicates);
+    RdbUtils::IsNull(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::IsNotNull(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, IsNotNull(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::IsNotNull(item1, predicates);
+    RdbUtils::IsNotNull(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::In(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, In(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0);
+    auto item2 = CreateOperationItem(0, { "test_table", "John" });
+    auto item3 = CreateOperationItem(0, {}, { std::vector<std::string>{ "zhangSan" }, std::vector<int>{ 1, 2, 3 } });
+    auto item4 = CreateOperationItem(
+        0, { "test_table", 2 }, { std::vector<std::string>{ "zhangSan" }, std::vector<int>{ 1, 2, 3 } });
+    RdbUtils::In(item1, predicates);
+    RdbUtils::In(item2, predicates);
+    RdbUtils::In(item3, predicates);
+    RdbUtils::In(item4, predicates);
+}
+
+void RdbDataShareAdapterTest::NotIn(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, NotIn(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0);
+    auto item2 = CreateOperationItem(0, { "test_table", "John" });
+    auto item3 = CreateOperationItem(0, {}, { std::vector<std::string>{ "zhangSan" }, std::vector<int>{ 1, 2, 3 } });
+    auto item4 = CreateOperationItem(
+        0, { "test_table", 2 }, { std::vector<std::string>{ "zhangSan" }, std::vector<int>{ 1, 2, 3 } });
+    RdbUtils::NotIn(item1, predicates);
+    RdbUtils::NotIn(item2, predicates);
+    RdbUtils::NotIn(item3, predicates);
+    RdbUtils::NotIn(item4, predicates);
+}
+
+void RdbDataShareAdapterTest::Like(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, Like(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::Like(item1, predicates);
+    RdbUtils::Like(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::NotLike(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, NotLike(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::NotLike(item1, predicates);
+    RdbUtils::NotLike(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::OrderByAsc(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, OrderByAsc(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::OrderByAsc(item1, predicates);
+    RdbUtils::OrderByAsc(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::OrderByDesc(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, OrderByDesc(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::OrderByDesc(item1, predicates);
+    RdbUtils::OrderByDesc(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::Offset(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, Offset(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::Offset(item1, predicates);
+    RdbUtils::Offset(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::BeginsWith(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, BeginsWith(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::BeginsWith(item1, predicates);
+    RdbUtils::BeginsWith(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::EndsWith(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, EndsWith(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::EndsWith(item1, predicates);
+    RdbUtils::EndsWith(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::GroupBy(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, GroupBy(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0);
+    auto item2 = CreateOperationItem(0, {}, { std::vector<std::string>{ "zhangSan" }, std::vector<int>{ 1, 2, 3 } });
+    RdbUtils::GroupBy(item1, predicates);
+    RdbUtils::GroupBy(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::IndexedBy(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, IndexedBy(testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::IndexedBy(item1, predicates);
+    RdbUtils::IndexedBy(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::Contains(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, Contains(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::Contains(item1, predicates);
+    RdbUtils::Contains(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::NotContains(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, NotContains(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::NotContains(item1, predicates);
+    RdbUtils::NotContains(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::Glob(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, Glob(testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", "John" });
+    RdbUtils::Glob(item1, predicates);
+    RdbUtils::Glob(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::Between(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, Between(testing::_, testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", 1, true });
+    RdbUtils::Between(item1, predicates);
+    RdbUtils::Between(item2, predicates);
+}
+
+void RdbDataShareAdapterTest::NotBetween(MockRdbPredicates &predicates)
+{
+    EXPECT_CALL(predicates, NotBetween(testing::_, testing::_, testing::_)).Times(1);
+    auto item1 = CreateOperationItem(0, {});
+    auto item2 = CreateOperationItem(0, { "name", 1, true });
+    RdbUtils::NotBetween(item1, predicates);
+    RdbUtils::NotBetween(item2, predicates);
+}
+
+/**
+ * @tc.name: RdbUtils_Operation_Test
+ * @tc.desc: Test all operation handlers in RdbUtils
+ * @tc.type: FUNC
+ */
+HWTEST_F(RdbDataShareAdapterTest, RdbUtils_Operation_Test, TestSize.Level1)
+{
+    LOG_INFO("RdbUtils_Operation_Test start");
+    MockRdbPredicates predicates("test_table");
+    LessThanTest(predicates);
+    GreaterThanOrEqualTo(predicates);
+    LessThanOrEqualTo(predicates);
+    IsNull(predicates);
+    IsNotNull(predicates);
+    In(predicates);
+    NotIn(predicates);
+    Like(predicates);
+    NotLike(predicates);
+    OrderByAsc(predicates);
+    OrderByDesc(predicates);
+    Offset(predicates);
+    BeginsWith(predicates);
+    EndsWith(predicates);
+    GroupBy(predicates);
+    IndexedBy(predicates);
+    Contains(predicates);
+    NotContains(predicates);
+    Glob(predicates);
+    Between(predicates);
+    NotBetween(predicates);
 }
