820c93f0a885fd53edd2163666e609a542f9b8bb
lcaidm <lichen139@huawei.com>
2025-04-07 11:18:42 +0800

    bugfix Signed-off-by:lcaidm<lichen139@huawei.com>
    Signed-off-by: lcaidm <lichen139@huawei.com>

--- interfaces/inner_api/rdb/mock/include/rdb_store_config.h
+++ interfaces/inner_api/rdb/mock/include/rdb_store_config.h
@@ -329,6 +329,8 @@ public:
     void SetRegisterInfo(RegisterType type, bool state) const;
     bool GetRegisterInfo(RegisterType type) const;
     bool IsEqualRegisterInfo(const RdbStoreConfig& config) const;
+    void SetKnowledgeProcessing(bool knowledgeProcessing);
+    bool GetKnowledgeProcessing() const;
 
 private:
     void ClearEncryptKey();
@@ -343,6 +345,7 @@ private:
     bool isVector_ = false;
     bool autoRekey_ = false;
     bool localOnly_ = false;
+    bool knowledgeProcessing_ = false;
     int32_t journalSize_;
     int32_t pageSize_;
     int32_t readConSize_ = 4;
