b0ecf2e5716076600524ea973029c5cec6b34ef3
邓俊 <dengjun39@huawei.com>
2025-04-24 22:52:42 +0800

    feat:新增日志订阅功能
    Signed-off-by: 邓俊 <dengjun39@huawei.com>

--- test/native/rdb/unittest/rdb_store_subscribe_test.cpp
+++ test/native/rdb/unittest/rdb_store_subscribe_test.cpp
@@ -14,6 +14,7 @@
  */
 #include <gtest/gtest.h>
 
+#include <block_data.h>
 #include <functional>
 #include <string>
 
@@ -22,14 +23,26 @@
 #include "rdb_helper.h"
 #include "rdb_open_callback.h"
 #include "rdb_sql_statistic.h"
+#include "rdb_sql_log.h"
 #include "rdb_store_manager.h"
 #include "rdb_types.h"
+#include "executor_pool.h"
+#include "shared_block.h"
+#include "sqlite_shared_result_set.h"
+#include "step_result_set.h"
 
 using namespace testing::ext;
 using namespace OHOS::NativeRdb;
 using namespace OHOS::DistributedRdb;
 using CheckOnChangeFunc = std::function<void(RdbStoreObserver::ChangeInfo &changeInfo)>;
 using SqlStatisticsFunc = std::function<void(SqlObserver::SqlExecutionInfo &info)>;
+using OnErrorObserverFunc = std::function<void(const SqlErrorObserver::ExceptionMessage &message)>;
+static constexpr int32_t MAX_THREAD = 5;
+static constexpr int32_t MIN_THREAD = 0;
+static std::string g_createTable = "CREATE TABLE IF NOT EXISTS test "
+"(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+"name TEXT NOT NULL, age INTEGER, salary "
+"REAL, blobType BLOB)";
 class SubObserver : public RdbStoreObserver {
 public:
     virtual ~SubObserver()
@@ -44,6 +57,17 @@ public:
 
 private:
     CheckOnChangeFunc checkOnChangeFunc_;
+};
+
+class OnErrorObserver : public SqlErrorObserver {
+public:
+    void OnErrorLog(const ExceptionMessage &message) override;
+    ExceptionMessage GetLastMessage();
+    void SetBlockData(std::shared_ptr<OHOS::BlockData<bool>> block);
+
+private:
+    std::shared_ptr<OHOS::BlockData<bool>> block_;
+    ExceptionMessage lastMessage;
 };
 
 class StatisticsObserver : public SqlObserver {
@@ -144,11 +168,7 @@ void SubObserver::OnChange(const Origin &origin, const PrimaryFields &fields, Rd
 void SubObserver::OnChange()
 {
     count++;
-    const std::string CREATE_TABLE_TEST = "CREATE TABLE IF NOT EXISTS test "
-                                          "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                          "name TEXT NOT NULL, age INTEGER, salary "
-                                          "REAL, blobType BLOB)";
-    RdbStoreSubTest::store->ExecuteSql(CREATE_TABLE_TEST);
+    RdbStoreSubTest::store->ExecuteSql(g_createTable);
     ValuesBucket values;
     int64_t id;
     values.PutInt("id", count);
@@ -164,6 +184,24 @@ void SubObserver::OnChange()
 void SubObserver::RegisterCallback(const CheckOnChangeFunc &callback)
 {
     checkOnChangeFunc_ = callback;
+}
+
+void OnErrorObserver::OnErrorLog(const ExceptionMessage &message)
+{
+    lastMessage = message;
+    if (block_) {
+        block_->SetValue(true);
+    }
+}
+
+ExceptionMessage OnErrorObserver::GetLastMessage()
+{
+    return lastMessage;
+}
+ 
+void OnErrorObserver::SetBlockData(std::shared_ptr<OHOS::BlockData<bool>> block)
+{
+    block_ = block;
 }
 
 void StatisticsObserver::OnStatistic(const StatisticsObserver::SqlExecutionInfo &info)
@@ -996,4 +1034,282 @@ HWTEST_F(RdbStoreSubTest, RdbStore_RegisterAutoSyncCallbackAndRdbStore_Unregiste
 
     status = store->UnregisterAutoSyncCallback(obs);
     EXPECT_EQ(status, E_OK);
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog001
+ * @tc.desc: test errorlog observer when SQLITE_ERROR
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog001, TestSize.Level1)
+{
+    auto observer = std::make_shared<OnErrorObserver>();
+    SqlLog::Subscribe(MAIN_DATABASE_NAME, observer);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    observer->SetBlockData(block);
+    ValuesBucket values;
+    int64_t id;
+    values.PutInt("id", 1);
+    values.PutString("name", std::string("zhangsan"));
+    values.PutInt("age", 18);
+    auto ret = store->Insert(id, "sqliteLog", values);
+    EXPECT_NE(ret, E_OK);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 1);
+    EXPECT_EQ(observer->GetLastMessage().message, "no such table: sqliteLog");
+    EXPECT_EQ(observer->GetLastMessage().sql, "INSERT INTO sqliteLog(age,id,name) VALUES (?,?,?)");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog002
+ * @tc.desc: test errorlog observer when SQLITE_CONSTRAINT
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog002, TestSize.Level1)
+{
+    auto observer = std::make_shared<OnErrorObserver>();
+    constexpr const char *createTableTest = "CREATE TABLE IF NOT EXISTS errorlog_test "
+                                        "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                        "name TEXT NOT NULL, age INTEGER)";
+    auto ret = store->ExecuteSql(createTableTest);
+    EXPECT_EQ(ret, E_OK);
+    ValuesBucket values;
+    int64_t id;
+    values.PutInt("id", 1);
+    values.PutString("name", std::string("zhangsan"));
+    values.PutInt("age", 18);
+    ret = store->Insert(id, "errorlog_test", values);
+    EXPECT_EQ(ret, E_OK);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    SqlLog::Subscribe(MAIN_DATABASE_NAME, observer);
+    observer->SetBlockData(block);
+    ret = store->Insert(id, "errorlog_test", values);
+    EXPECT_NE(ret, E_OK);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 19);
+    EXPECT_EQ(observer->GetLastMessage().message, "UNIQUE constraint failed: errorlog_test.id");
+    EXPECT_EQ(observer->GetLastMessage().sql, "INSERT INTO errorlog_test(age,id,name) VALUES (?,?,?)");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog003
+ * @tc.desc: test errorlog observer when SQLITE_BUSY
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog003, TestSize.Level1)
+{
+    auto observer = std::make_shared<OnErrorObserver>();
+    std::string tableName = "OnErrorLogTable";
+    auto res = store->Execute(
+        "CREATE TABLE " + tableName + " (id INTEGER PRIMARY KEY CHECK (id >= 3 OR id <= 1), name TEXT NOT NULL)");
+    ASSERT_EQ(res.first, E_OK);
+
+    auto [code, transaction] = store->CreateTransaction(Transaction::IMMEDIATE);
+    ASSERT_EQ(code, E_OK);
+    ASSERT_NE(transaction, nullptr);
+
+    ValuesBuckets rows;
+    for (int i = 0; i < 5; i++) {
+        ValuesBucket row;
+        row.Put("id", i);
+        row.Put("name", "Jim");
+        rows.Put(row);
+    }
+    SqlLog::Subscribe(MAIN_DATABASE_NAME, observer);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    observer->SetBlockData(block);
+    auto result = store->BatchInsertWithConflictResolution(tableName, rows, ConflictResolution::ON_CONFLICT_NONE);
+    ASSERT_EQ(result.first, E_SQLITE_BUSY);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 5);
+    EXPECT_EQ(observer->GetLastMessage().message, "database is locked");
+    EXPECT_EQ(
+        observer->GetLastMessage().sql, "INSERT INTO OnErrorLogTable (id,name) VALUES (?,?),(?,?),(?,?),(?,?),(?,?)");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog004
+ * @tc.desc: test errorlog observer when SQLITE_LOCKED
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+ HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog004, TestSize.Level1)
+ {
+    auto observer = std::make_shared<OnErrorObserver>();
+    std::string dbPath = RDB_TEST_PATH + "errorlog.db";
+    int errCode = E_OK;
+    RdbStoreConfig memoryConfig(dbPath);
+    Callback helper;
+    memoryConfig.SetStorageMode(StorageMode::MODE_MEMORY);
+    std::shared_ptr<RdbStore> memoryStore = RdbHelper::GetRdbStore(memoryConfig, 1, helper, errCode);
+    auto res = memoryStore->Execute("CREATE TABLE test1 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)");
+    ASSERT_EQ(res.first, E_OK);
+    auto [ret1, transaction1] = memoryStore->CreateTransaction(Transaction::IMMEDIATE);
+    ASSERT_EQ(ret1, E_OK);
+    ASSERT_NE(transaction1, nullptr);
+ 
+    auto [ret, transaction] = memoryStore->CreateTransaction(Transaction::DEFERRED);
+    ASSERT_EQ(ret, E_OK);
+    ASSERT_NE(transaction, nullptr);
+ 
+    ValuesBuckets rows;
+    for (int i = 0; i < 5; i++) {
+        Transaction::Row row;
+        row.Put("id", i);
+        row.Put("name", "Jim_batchInsert");
+        rows.Put(row);
+    }
+    SqlLog::Subscribe(dbPath, observer);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    observer->SetBlockData(block);
+    auto result = transaction->BatchInsertWithConflictResolution("test1", rows, ConflictResolution::ON_CONFLICT_NONE);
+    ASSERT_EQ(result.first, E_SQLITE_LOCKED);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 6);
+    EXPECT_EQ(observer->GetLastMessage().message, "database table is locked");
+    EXPECT_EQ(observer->GetLastMessage().sql, "INSERT INTO test1 (id,name) VALUES (?,?),(?,?),(?,?),(?,?),(?,?)");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog005
+ * @tc.desc: test errorlog observer when SQLITE_SCHEMA
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog005, TestSize.Level1)
+{
+    auto observer = std::make_shared<OnErrorObserver>();
+    store->ExecuteSql(g_createTable);
+    std::shared_ptr<OHOS::ExecutorPool> executors;
+    std::shared_ptr<OHOS::BlockData<int32_t>> block1 = std::make_shared<OHOS::BlockData<int32_t>>(3, false);
+    executors = std::make_shared<OHOS::ExecutorPool>(MAX_THREAD, MIN_THREAD);
+    auto taskId1 = executors->Execute([storeCopy = store, block1]() {
+        constexpr const char *createTable = "CREATE TABLE test";
+        constexpr const char *createTableColumn = " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                                  "name TEXT NOT NULL, age INTEGER, salary REAL, "
+                                                  "blobType BLOB)";
+        int32_t errCode = E_ERROR;
+        for (uint32_t i = 0; i < 2000; i++) {
+            errCode = storeCopy->ExecuteSql(createTable + std::to_string(i) + createTableColumn);
+            if (errCode != E_OK) {
+                break;
+            }
+        }
+        block1->SetValue(errCode);
+    });
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    SqlLog::Subscribe(MAIN_DATABASE_NAME, observer);
+    observer->SetBlockData(block);
+    std::shared_ptr<OHOS::BlockData<int32_t>> block2 = std::make_shared<OHOS::BlockData<int32_t>>(3, false);
+    auto taskId2 = executors->Execute([storeCopy = store, block2]() {
+        int32_t errCode = E_ERROR;
+        for (uint32_t i = 0; i < 2000; i++) {
+            auto resultSet = storeCopy->QueryByStep("SELECT * FROM test");
+            int rowCount = -1;
+            errCode = resultSet->GetRowCount(rowCount);
+            resultSet->Close();
+            if (errCode != E_OK && errCode != E_SQLITE_SCHEMA) {
+                break;
+            }
+        }
+        auto code = (errCode == E_OK || errCode == E_SQLITE_SCHEMA) ? E_OK : errCode;
+        block2->SetValue(code);
+    });
+    EXPECT_EQ(block1->GetValue(), E_OK);
+    EXPECT_EQ(block2->GetValue(), E_OK);
+    EXPECT_NE(taskId1, taskId2);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 17);
+    EXPECT_EQ(observer->GetLastMessage().message, "database schema has changed");
+    EXPECT_EQ(observer->GetLastMessage().sql, "SELECT * FROM test");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog006
+ * @tc.desc: test errorlog observer when SQLITE_FULL
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog006, TestSize.Level1)
+{
+    auto observer = std::make_shared<OnErrorObserver>();
+    store->ExecuteSql(g_createTable);
+    auto [code, maxPageCount] = store->Execute("PRAGMA max_page_count;");
+    auto recover = std::shared_ptr<const char>("recover", [defPageCount = maxPageCount,
+        storeCopy = store](const char *) {
+        storeCopy->Execute("PRAGMA max_page_count = " + static_cast<std::string>(defPageCount) + ";");
+    });
+    std::tie(code, maxPageCount) = store->Execute("PRAGMA max_page_count = 256;");
+    ValuesBucket row;
+    row.Put("name", std::string(1024 * 1024, 'e'));
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    SqlLog::Subscribe(MAIN_DATABASE_NAME, observer);
+    observer->SetBlockData(block);
+    auto result = store->Insert("test", row, ConflictResolution::ON_CONFLICT_NONE);
+    ASSERT_EQ(result.first, E_SQLITE_FULL);
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 13);
+    EXPECT_EQ(observer->GetLastMessage().message, "database or disk is full");
+    EXPECT_EQ(observer->GetLastMessage().sql, "INSERT INTO test(name) VALUES (?)");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog007
+ * @tc.desc: test errorlog observer when Notify
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog007, TestSize.Level1)
+{
+    std::string storeId = "test_db";
+    auto observer = std::make_shared<OnErrorObserver>();
+    SqlLog::Subscribe(storeId, observer);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    observer->SetBlockData(block);
+
+    SqlErrorObserver::ExceptionMessage error;
+    error.code = 2;
+    error.message = "concurrent error";
+    error.sql = "UPDATE table SET invalid";
+
+    SqlLog::Notify(storeId, error);
+
+    EXPECT_TRUE(block->GetValue());
+    EXPECT_EQ(observer->GetLastMessage().code, 2);
+    EXPECT_EQ(observer->GetLastMessage().message, "concurrent error");
+    EXPECT_EQ(observer->GetLastMessage().sql, "UPDATE table SET invalid");
+}
+
+/**
+ * @tc.name: RdbStoreSubscribeLog008
+ * @tc.desc: test errorlog observer when off observer
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RdbStoreSubTest, RdbStoreSubscribeLog008, TestSize.Level1)
+{
+    std::string storeId = "test_db";
+    auto observer = std::make_shared<OnErrorObserver>();
+    SqlLog::Subscribe(storeId, observer);
+    std::shared_ptr<OHOS::BlockData<bool>> block = std::make_shared<OHOS::BlockData<bool>>(3, false);
+    observer->SetBlockData(block);
+    SqlLog::Unsubscribe(storeId, observer);
+    SqlErrorObserver::ExceptionMessage error;
+    error.code = 2;
+    error.message = "concurrent error";
+    error.sql = "UPDATE table SET invalid";
+    SqlLog::Notify(storeId, error);
+    EXPECT_EQ(block->GetValue(), false);
+    EXPECT_EQ(observer->GetLastMessage().code, 0);
 }
