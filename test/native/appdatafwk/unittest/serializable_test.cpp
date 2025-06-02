837dd8be485a14310c8c0f013a62f8d03d4644b0
bjd <baijidong@huawei.com>
2025-06-02 10:43:33 +0800

    部件化整改
    Signed-off-by: bjd <baijidong@huawei.com>

--- test/native/appdatafwk/unittest/serializable_test.cpp
+++ test/native/appdatafwk/unittest/serializable_test.cpp
@@ -127,7 +127,10 @@ HWTEST_F(SerializableTest, GetNormalVal, TestSize.Level2)
     normal.value = -56;
     normal.isClear = true;
     normal.cols = { "adfasdfas" };
-    auto jstr = Serializable::JSONWrapper::to_string(normal.Marshall());
+    auto json = normal.Marshall();
+    auto jstr = Serializable::JSONWrapper::to_string(json);
+    EXPECT_TRUE(json["count"] == -1);
+    EXPECT_TRUE(json["name"] == "normal");
     Normal normal1;
     normal1.Unmarshall(jstr);
     ASSERT_TRUE(normal == normal1) << normal1.name;
