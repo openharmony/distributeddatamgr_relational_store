f2cd14689b14623336e362ad44db58077e4348ab
bjd <baijidong@huawei.com>
2025-06-11 10:08:03 +0800

    告警修改
    Signed-off-by: bjd <baijidong@huawei.com>

--- frameworks/native/appdatafwk/src/serializable.cpp
+++ frameworks/native/appdatafwk/src/serializable.cpp
@@ -345,8 +345,24 @@ Serializable::JSONWrapper &Serializable::JSONWrapper::operator=(int32_t value)
 
 Serializable::JSONWrapper &Serializable::JSONWrapper::operator=(uint32_t value)
 {
-    int32_t number = static_cast<int32_t>(value);
-    return operator=(number);
+    if (root_ == nullptr && cJSON_IsNull(json_)) {
+        cJSON_Delete(json_);
+        json_ = nullptr;
+    }
+    if (json_ != nullptr) {
+        if (cJSON_IsNumber(json_)) {
+            cJSON_SetNumberValue(json_, static_cast<double>(value));
+        } else {
+            LOG_WARN("type is not number");
+        }
+        return *this;
+    }
+    json_ = cJSON_CreateNumber(static_cast<double>(value));
+    if (json_ == nullptr || root_ == nullptr) {
+        return *this;
+    }
+    AddToRoot();
+    return *this;
 }
 
 Serializable::JSONWrapper &Serializable::JSONWrapper::operator=(int64_t value)
