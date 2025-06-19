e7d36bb04590428a7f3376e85fb28d60d3f032bb
wangdengze <wangdengze@huawei.com>
2025-06-19 17:12:51 +0800

    “modify”
    Signed-off-by: wangdengze <wangdengze@huawei.com>

--- frameworks/native/rdb/src/grd_api_manager.cpp
+++ frameworks/native/rdb/src/grd_api_manager.cpp
@@ -62,7 +62,7 @@ void GRD_DBApiInitEnhance(GRD_APIInfo &GRD_DBApiInfo)
     GRD_DBApiInfo.DBReKeyApi = (DBReKey)dlsym(g_library, "GRD_DBRekey");
     GRD_DBApiInfo.DBGetConfigApi = (DBGetConfig)dlsym(g_library, "GRD_GetConfig");
     GRD_DBApiInfo.DBSetConfigApi = (DBSetConfig)dlsym(g_library, "GRD_SetConfig");
-    GRD_DBApiInfo.DBSqlRegistryThreadPool = (DBSqlRegistryThreadPool)dlsym(g_library, "GRD_SqlRegistryThreadPool");
+    GRD_DBApiInfo.DBSqlRegistryThreadPool = (DBSqlRegistryThreadPool)dlsym(g_library, "GRD_DbRegistryThreadPool");
     GRD_DBApiInfo.DBSqlRegistryClusterAlgo = (DBSqlRegistryClusterAlgo)dlsym(g_library, "GRD_SqlRegistryClusterAlgo");
 #endif
 }
