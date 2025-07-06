24fd5c817610bcd2cd5ccb21df11475030cd9db2
libo <libo512@huawei-partners.com>
2025-07-06 22:20:26 +0800

    relationalstore arkui-x 适配跨平台修改
    Signed-off-by: BrainL <libuyan1@huawei.com>

--- frameworks/js/napi/common/mock/include/mock.h
+++ frameworks/js/napi/common/mock/include/mock.h
@@ -1,5 +1,19 @@
-#ifndef SEND_EVENT_H
-#define SEND_EVENT_H
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef SEND_EVENT_MOCK_H
+#define SEND_EVENT_MOCK_H
 #include <functional>
 #include "event_handler.h"
 #include "napi/native_api.h"
@@ -11,4 +25,4 @@ napi_status SendEventMock(napi_env env,
                         const std::function<void()>& cb,
                         napi_event_priority priority,
                         const char* name);
-#endif //MAC_NAPI_H
+#endif //SEND_EVENT_MOCK_H
