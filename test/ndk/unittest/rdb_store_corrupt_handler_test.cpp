922d17211af38668d95f6663f896d27ed02765f8
baijidong <baijidong@huawei.com>
2025-09-22 21:09:28 +0800

    ndk 增加自定义处理损坏接口
    Signed-off-by: baijidong <baijidong@huawei.com>

--- test/ndk/unittest/rdb_store_corrupt_handler_test.cpp
+++ test/ndk/unittest/rdb_store_corrupt_handler_test.cpp
@@ -12,11 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include
 #include <gtest/gtest.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include
+#include <string>
 
 #include "accesstoken_kit.h"
 #include "common.h"
