9df349874fe1da2a2dc7d4d4f151021f31d352e8
baijidong <baijidong@huawei.com>
2025-10-13 20:41:48 +0800

    add rekeyEx
    Signed-off-by: baijidong <baijidong@huawei.com>

--- rdbmock/frameworks/native/rdb/securec/securec.h
+++ rdbmock/frameworks/native/rdb/securec/securec.h
@@ -15,8 +15,8 @@
 
 #ifndef RELATIONAL_STORE_SECUREC_H
 #define RELATIONAL_STORE_SECUREC_H
-
-static errno_t memcpy_s(void *dest, size_t destMax, const void *src, size_t count)
+#include <cstring>
+errno_t memcpy_s(void *dest, size_t destMax, const void *src, size_t count)
 {
     memcpy(dest, src, count);
     return 0;
