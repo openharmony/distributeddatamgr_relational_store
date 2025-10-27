3a812f1bebc158958fae72d8d299b23996279414
baijidong <baijidong@huawei.com>
2025-10-27 11:35:52 +0800

    fix rekey
    Signed-off-by: baijidong <baijidong@huawei.com>

--- frameworks/native/rdb/src/sqlite_connection.cpp
+++ frameworks/native/rdb/src/sqlite_connection.cpp
@@ -813,19 +813,19 @@ int SqliteConnection::Rekey(const RdbStoreConfig::CryptoParam &cryptoParam)
         LOG_ERROR("key is empty");
         return E_ERROR;
     }
-    errCode = sqlite3_rekey(dbHandle_, static_cast<const void *>(key.data()), static_cast<int>(key.size()));
-    if (errCode != SQLITE_OK) {
-        key.assign(key.size(), 0);
-        LOG_ERROR("ReKey failed, err = %{public}d, name = %{public}s", errCode,
-            SqliteUtils::Anonymous(config_.GetName()).c_str());
-        return SQLiteError::ErrNo(errCode);
-    }
     errCode = ExecuteSql(std::string(GlobalExpr::REKEY_HMAC_ALGO) +
                               SqliteUtils::HmacAlgoDescription(cryptoParam.hmacAlgo) +
                               std::string(GlobalExpr::ALGO_SUFFIX));
     if (errCode != E_OK) {
         LOG_ERROR("set codec hmac algo failed, err = %{public}d", errCode);
         return errCode;
+    }
+    errCode = sqlite3_rekey(dbHandle_, static_cast<const void *>(key.data()), static_cast<int>(key.size()));
+    if (errCode != SQLITE_OK) {
+        key.assign(key.size(), 0);
+        LOG_ERROR("ReKey failed, err = %{public}d, name = %{public}s", errCode,
+            SqliteUtils::Anonymous(config_.GetName()).c_str());
+        return SQLiteError::ErrNo(errCode);
     }
     errCode = SetEncryptAgo(cryptoParam);
     if (errCode != E_OK) {
