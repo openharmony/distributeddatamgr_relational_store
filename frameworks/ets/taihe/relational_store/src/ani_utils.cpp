9d0ee3ac5a30159e7b421bc95c183dc65bf69227
Ivan Tyulyandin <ivan.tyulyandin@huawei.com>
2025-10-27 12:10:07 +0800

    Move Record and BigInt to std.core
    Issue: https://gitcode.com/openharmony/arkcompiler_runtime_core/issues/7690
    
    Testing: all pre-merge tests passed
    
    Signed-off-by: ivan-tyulyandin <ivan.tyulyandin@huawei.com>

--- frameworks/ets/taihe/relational_store/src/ani_utils.cpp
+++ frameworks/ets/taihe/relational_store/src/ani_utils.cpp
@@ -570,7 +570,7 @@ ani_ref UnionAccessor::AniIteratorNext(ani_ref interator, bool &isSuccess)
 template<>
 bool UnionAccessor::TryConvert<ValuesBucket>(ValuesBucket &value)
 {
-    if (!IsInstanceOf("escompat.Record")) {
+    if (!IsInstanceOf("std.core.Record")) {
         return false;
     }
     ani_ref keys;
