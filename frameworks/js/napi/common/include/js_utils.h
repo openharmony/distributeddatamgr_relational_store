795a0dc0b0bd299fb42af0c53de12a333c6788e6
bigteer <zhangdi126@huawei.com>
2026-03-07 16:38:23 +0800

    update GetNamedProperty
    Signed-off-by: bigteer <zhangdi126@huawei.com>

--- frameworks/js/napi/common/include/js_utils.h
+++ frameworks/js/napi/common/include/js_utils.h
@@ -93,9 +93,6 @@ int32_t Convert2Value(napi_env env, napi_value jsValue, std::vector<T> &value);
 template<typename T>
 int32_t Convert2Value(napi_env env, napi_value jsValue, std::map<std::string, T> &value, bool isAllowEmpty = false);
 
-template<typename T>
-int32_t Convert2Value(napi_env env, napi_value jsValue, std::optional<T> &value);
-
 template<typename... Types>
 int32_t Convert2Value(napi_env env, napi_value jsValue, std::variant<Types...> &value);
 
@@ -321,18 +318,6 @@ int32_t JSUtils::Convert2Value(napi_env env, napi_value jsValue, std::map<std::s
         }
         value.insert(std::pair<std::string, T>(key, val));
     }
-    return napi_ok;
-}
-
-template<typename T>
-int32_t JSUtils::Convert2Value(napi_env env, napi_value jsValue, std::optional<T> &value)
-{
-    T val;
-    auto ret = Convert2Value(env, jsValue, val);
-    if (ret != napi_ok) {
-        return napi_invalid_arg;
-    }
-    value = val;
     return napi_ok;
 }
 
