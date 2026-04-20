32b9502b1faefbd0180db620034d5e2ee4c56d83
邓俊 <dengjun39@huawei.com>
2026-04-20 17:13:04 +0800

    fix:修改检视意见
    Signed-off-by: 邓俊 <dengjun39@huawei.com>

--- frameworks/ets/taihe/relational_store/src/rdb_store_impl.cpp
+++ frameworks/ets/taihe/relational_store/src/rdb_store_impl.cpp
@@ -1053,7 +1053,7 @@ void RdbStoreImpl::SyncEx(SyncMode mode, weak::RdbPredicates predicates, uintptr
         std::make_shared<InnerError>(OHOS::NativeRdb::E_ALREADY_CLOSED), RDB_DO_NOTHING);
     auto rdbPredicateNative = ani_rdbutils::GetNativePredicatesFromTaihe(predicates);
     ASSERT_RETURN_THROW_ERROR(rdbPredicateNative != nullptr,
-        std::make_shared<InnerError>(OHOS::NativeRdb::E_INVALID_ARGS_NEW), RDB_DO_NOTHING);
+        std::make_shared<ParamError>("predicates", "an RdbPredicates."), RDB_DO_NOTHING);
     auto nativeMode = ani_rdbutils::SyncModeToNative(mode);
     ASSERT_RETURN_THROW_ERROR(
         (nativeMode == OHOS::DistributedRdb::SyncMode::PUSH) || (nativeMode == OHOS::DistributedRdb::SyncMode::PULL),
@@ -1062,7 +1062,7 @@ void RdbStoreImpl::SyncEx(SyncMode mode, weak::RdbPredicates predicates, uintptr
     OHOS::DistributedRdb::SyncOption option{ nativeMode, false, true};
     std::shared_ptr<AniContext> context = std::make_shared<AniContext>();
     if (!context->Init(callback)) {
-        ThrowInnerErrorExt(OHOS::NativeRdb::E_ERROR);  // 错误码存在问题
+        ThrowInnerErrorExt(OHOS::NativeRdb::E_ERROR);
         return;
     }
     promise = context->promise_;
@@ -1078,9 +1078,9 @@ void RdbStoreImpl::SyncEx(SyncMode mode, weak::RdbPredicates predicates, uintptr
             context->error_ = std::make_shared<InnerError>(NativeRdb::E_ERROR);
         }
         AniAsyncCall::ReturnResult(context, callbackEnv);
-    }; 
+    };
+
     int errCode = store->SyncEx(option, *rdbPredicateNative, nativeSyncCallback);
-    LOG_ERROR("---dj--- taihe SyncEx ddddddddddd errCode:%{public}d.", errCode);
     if (errCode != OHOS::NativeRdb::E_OK) {
         context->error_ = std::make_shared<InnerError>(errCode);
         AniAsyncCall::ReturnResult(context, env);
