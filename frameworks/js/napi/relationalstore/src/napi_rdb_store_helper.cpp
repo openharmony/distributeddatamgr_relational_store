f58b4af3e7443f8f0a9c8385490770f7a0200186
chang-jiaxing <changjiaxing2@h-partners.com>
2026-04-29 20:26:34 +0800

    refactor: GetRdbStoreSync直接使用HistogramReporter对象
    去掉std::optional包装，GetRdbStoreSync是同步函数无需optional。
    宏去掉has_value检查，直接调用SetErrCode。
    
    Co-Authored-By: Agent
    Signed-off-by: chang-jiaxing <changjiaxing1@huawei.com>
    Change-Id: I513d26d0e584dfedfb3ed8eac90458547cecb56a

--- frameworks/js/napi/relationalstore/src/napi_rdb_store_helper.cpp
+++ frameworks/js/napi/relationalstore/src/napi_rdb_store_helper.cpp
@@ -126,8 +126,7 @@ napi_value GetRdbStore(napi_env env, napi_callback_info info)
 
 napi_value GetRdbStoreSync(napi_env env, napi_callback_info info)
 {
-    std::optional<NativeRdb::HistogramReporter> histogram;
-    histogram.emplace("Arkdata.Rdb.RdbStore.getRdbStoreSync",
+    NativeRdb::HistogramReporter histogram("Arkdata.Rdb.RdbStore.getRdbStoreSync",
         HistogramType::TIME | HistogramType::BOOL | HistogramType::ENUM,
         std::chrono::steady_clock::now(), true);
     auto context = std::make_shared<GetRdbStoreContext>();
@@ -171,7 +170,7 @@ napi_value GetRdbStoreSync(napi_env env, napi_callback_info info)
     DefaultOpenCallback callback;
     context->proxy =
         RdbHelper::GetRdbStore(GetRdbStoreConfig(context->config, context->param), -1, callback, errCode);
-    histogram->SetErrCode(errCode);
+    histogram.SetErrCode(errCode);
     RDB_NAPI_ASSERT_HV(env, errCode == OK, histogram, std::make_shared<InnerErrorExt>(errCode));
 
     napi_value result = RdbStoreProxy::NewInstance(env, context->proxy, context->param.isSystemApp);
