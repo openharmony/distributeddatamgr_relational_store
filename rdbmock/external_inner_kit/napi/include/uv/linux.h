c06a79b7522f077151b47620d33cd317d4b26cd7
guochao <guochao76@h-partners.com>
2026-06-04 19:08:49 +0800

    feat: add rdbmock ipc mock impl and napi small headers
    Fixes #2608
    
    Signed-off-by: guochao95 <guochao76@h-partners.com>
    
    Co-Authored-By: Agent

---
+++ rdbmock/external_inner_kit/napi/include/uv/linux.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021-2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef UV_LINUX_H
+#define UV_LINUX_H
+
+#define UV_PLATFORM_LOOP_FIELDS                                               \
+  uv__io_t inotify_read_watcher;                                              \
+  void* inotify_watchers;                                                     \
+  int inotify_fd;                                                             \
+
+#define UV_PLATFORM_FS_EVENT_FIELDS                                           \
+  struct uv__queue watchers;                                                  \
+  int wd;                                                                     \
+
+#endif /* UV_LINUX_H */
